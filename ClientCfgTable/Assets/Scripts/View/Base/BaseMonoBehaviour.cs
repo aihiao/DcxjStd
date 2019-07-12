5c241e266794a21a02434c8cee35f2b011cf088a
liyangwei999 <1611086176@qq.com>
2019-07-12 15:23:24 +0800

    新增BaseMonoBehaviour类

---
+++ ClientCfgTable/Assets/Scripts/View/Base/BaseMonoBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class BaseMonoBehaviour : MonoBehaviour
+{
+    protected bool init;
+
+    public bool RunInitialize()
+    {
+        if (!init)
+        {
+            Initialize();
+            init = !init;
+        }
+        return init;
+    }
+
+    public virtual void Initialize() { }
+
+    public virtual void Dispose() { }
+
+    public virtual void Destroy()
+    {
+        init = false;
+    }
+
+}
