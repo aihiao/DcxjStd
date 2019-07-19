898bd5295722d07c9789a3557d2ed7bb3e40f295
liyangwei999 <1611086176@qq.com>
2019-07-19 10:38:28 +0800

    新增CommonUtility.cs

---
+++ ClientCfgTable/Assets/Scripts/Utility/CommonUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class CommonUtility
+{
+    public static bool IsNull(this object obj)
+    {
+        return obj == null;
+    }
+
+    public static bool NotNull(this object obj)
+    {
+        return obj != null;
+    }
+}
