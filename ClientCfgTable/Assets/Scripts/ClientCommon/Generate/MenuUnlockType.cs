f000fe0adb2a1a09adc5a43ebe09e926598d5f2e
liyangwei999 <1611086176@qq.com>
2019-07-19 10:36:37 +0800

    MenuUnlockType.cs

---
+++ ClientCfgTable/Assets/Scripts/ClientCommon/Generate/MenuUnlockType.cs
@@ -0,0 +1,42 @@
+﻿namespace ClientCommon
+{
+    public sealed class MenuUnlockType : TypeNameContainer<MenuUnlockType>
+    {
+        /// <summary>
+        /// VIP等级限制
+        /// </summary>
+        public const int VIPLevel = 0;
+        /// <summary>
+        /// 等级限制
+        /// </summary>
+        public const int Level = 1;
+        /// <summary>
+        /// 关卡限制功能解锁
+        /// </summary>
+        public const int Dungeon = 2;
+        /// <summary>
+        /// 关卡解锁关卡
+        /// </summary>
+        public const int DungeonUnlock = 3;
+        /// <summary>
+        /// 完成任务解锁
+        /// </summary>
+        public const int MissionUnlock = 4;
+
+        public static void Initialize()
+        {
+            if (initialized)
+                return;
+
+            SetTextSectionName("TypeDef_MenuUnlockType");
+
+            RegisterType("VIPLevel", VIPLevel, "VIP等级限制");
+            RegisterType("Level", Level, "等级限制");
+            RegisterType("Dungeon", Dungeon, "关卡限制功能解锁");
+            RegisterType("DungeonUnlock", DungeonUnlock, "关卡解锁关卡");
+            RegisterType("MissionUnlock", MissionUnlock, "完成任务解锁");
+
+            initialized = true;
+        }
+    }
+}
