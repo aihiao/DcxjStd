315abdbd4b4e37aa15963b4699e3a455eddfac68
liyangwei999 <1611086176@qq.com>
2019-07-19 12:01:34 +0800

    新增UiDialog.cs

---
+++ ClientCfgTable/Assets/Scripts/View/Views/Common/UiDialog.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+public class UiDialogBtn
+{
+    public const uint Ok = 1;
+    public const uint Yes = 2;
+    public const uint No = 4;
+    public const uint Cancel = 8;
+    public const uint Close = 16;
+    public const uint None = 0;
+}
+
+public class UiDialog : BaseUi
+{
+    public UiDialog Set(string contentText, uint btnFlag = UiDialogBtn.None, Action<AlertBtnType, object> callBack = null, bool destroyOnClick = true)
+    {
+        return null;
+    }
+
+    public UiDialog SetData(object obj)
+    {
+        return this;
+    }
+
+}
