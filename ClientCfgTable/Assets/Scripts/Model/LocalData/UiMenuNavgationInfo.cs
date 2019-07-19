c35f16f2fe8c92a04b2239d4b46af4e2d2a2172e
liyangwei999 <1611086176@qq.com>
2019-07-19 12:08:38 +0800

    新增UiMenuNavgationInfo.cs

---
+++ ClientCfgTable/Assets/Scripts/Model/LocalData/UiMenuNavgationInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class MenuParentId
+{
+    public const int NPCMenu = -2; // 父菜单Id=-2就是Npc中的功能按钮, 解锁的时候飞向NPC按钮
+    public const int CommonMenu = -3; // 父菜单Id=-3就是普通功能按钮, 解锁的时候飞向自己的位置, 并显示出来
+
+    public static bool IsMenuIgnore(int id)
+    {
+        return id == NPCMenu || id == CommonMenu;
+    }
+}
+
+public class UiMenuNavgationInfo
+{
+
+}
