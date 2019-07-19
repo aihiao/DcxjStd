51987bd7746a14f840db6f259b949d5adf983992
liyangwei999 <1611086176@qq.com>
2019-07-19 12:55:53 +0800

    新增DataModelManager.cs

---
+++ ClientCfgTable/Assets/Scripts/Managers/Data/DataModelManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class DataModelManager : AbsManager<DataModelManager>
+{
+
+    //多人副本
+    private TeamDungeonInfo teamDungeonInfo;
+    public TeamDungeonInfo TeamDungeonInfo
+    {
+        get
+        {
+            if (teamDungeonInfo == null)
+            {
+                teamDungeonInfo = new TeamDungeonInfo();
+            }
+            return teamDungeonInfo;
+        }
+    }
+
+    private UiMenuNavgationInfo menuNavgation;
+    public UiMenuNavgationInfo UIMenuNavgation
+    {
+        get
+        {
+            if (menuNavgation == null)
+            {
+                menuNavgation = new UiMenuNavgationInfo();
+            }
+            return menuNavgation;
+        }
+    }
+
+}
