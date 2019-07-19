38b7642b2fda9af11cb7d7701189caad8a8c5aab
liyangwei999 <1611086176@qq.com>
2019-07-19 10:28:40 +0800

    IdSeg Type

---
+++ ClientCfgTable/Assets/Scripts/ClientCommon/Generate/IdSeg.cs
@@ -0,0 +1,22 @@
+﻿namespace ClientCommon
+{
+    public sealed class IdSeg : TypeNameContainer<IdSeg>
+    {
+        /// <summary>
+        /// 无效ID标记
+        /// </summary>
+        public const int InvalidId = 0;
+
+        public static void Initialize()
+        {
+            if (initialized)
+                return;
+
+            SetTextSectionName("TypeDef_IdSeg");
+
+            RegisterType("InvalidId", InvalidId, "InvalidId");
+
+            initialized = true;
+        }
+    }
+}
