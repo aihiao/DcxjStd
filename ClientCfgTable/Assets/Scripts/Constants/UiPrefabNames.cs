54afc559d661d3e6274b3c54f03e96f669a08905
liyangwei999 <1611086176@qq.com>
2019-07-19 10:39:00 +0800

    新增UiPrefabNames.cs

---
+++ ClientCfgTable/Assets/Scripts/Constants/UiPrefabNames.cs
@@ -0,0 +1,6 @@
+﻿public class UiPrefabNames
+{
+    public const string UiPnlTextTip = "UiPnlTextTip";
+    public const string UiPnlHelp = "UiPnlHelp";
+    public const string UiPnlMessage = "UiPnlMessage";
+}
