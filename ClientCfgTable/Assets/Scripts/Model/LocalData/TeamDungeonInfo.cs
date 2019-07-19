fb2a815e0813142de730f8a077dc06afba425afc
liyangwei999 <1611086176@qq.com>
2019-07-19 12:51:43 +0800

    新增TeamDungeonInfo.cs

---
+++ ClientCfgTable/Assets/Scripts/Model/LocalData/TeamDungeonInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TeamDungeonInfo
+{
+    /// <summary>
+	/// 队伍人数
+	/// </summary
+	public const int GroupCount = 3;
+
+    /// <summary>
+    /// 是否在组队中
+    /// </summary>
+    private bool isInTeam = false;
+    public bool IsInTeam
+    {
+        get { return isInTeam; }
+    }
+
+    /// <summary>
+	/// 当前剩余奖励次数
+	/// </summary>
+	private int remainRewardCount = 0;
+    public int RemainRewardCount
+    {
+        set { remainRewardCount = value; }
+        get { return remainRewardCount; }
+    }
+
+    /// <summary>
+    /// 已经购买奖励次数
+    /// </summary>
+    private int alreadyBuyCount = 0;
+    public int AlreadyBuyCount
+    {
+        set { alreadyBuyCount = value; }
+        get { return alreadyBuyCount; }
+    }
+
+    /// <summary>
+    /// 剩余购买奖励次数
+    /// </summary>
+    private int remainBuyCount = 0;
+    public int RemainBuyCount
+    {
+        set { remainBuyCount = value; }
+        get { return remainBuyCount; }
+    }
+
+}
