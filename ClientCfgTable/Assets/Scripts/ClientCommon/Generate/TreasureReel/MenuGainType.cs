161df9cc790062792215b74ac8da109a73e8b90d
liyangwei999 <1611086176@qq.com>
2019-07-19 10:36:56 +0800

    MenuGainType.cs

---
+++ ClientCfgTable/Assets/Scripts/ClientCommon/Generate/TreasureReel/MenuGainType.cs
@@ -0,0 +1,42 @@
+﻿namespace ClientCommon
+{
+    public sealed class MenuGainType : TypeNameContainer<MenuGainType>
+    {
+        /// <summary>
+        /// 无跳转
+        /// </summary>
+        public const int None = 0;
+        /// <summary>
+        /// 功能跳转
+        /// </summary>
+        public const int Function = 1;
+        /// <summary>
+        /// 剧情副本关卡
+        /// </summary>
+        public const int DungeonPoint = 2;
+        /// <summary>
+        /// 剧情副本章节
+        /// </summary>
+        public const int DungeonChapater = 3;
+        /// <summary>
+        /// 子功能获取跳转
+        /// </summary>
+        public const int SubFunction = 4;
+
+        public static void Initialize()
+        {
+            if (initialized)
+                return;
+
+            SetTextSectionName("TypeDef_MenuGainType");
+
+            RegisterType("None", None, "无跳转");
+            RegisterType("Function", Function, "功能跳转");
+            RegisterType("DungeonPoint", DungeonPoint, "剧情副本关卡");
+            RegisterType("DungeonChapater", DungeonChapater, "剧情副本章节");
+            RegisterType("SubFunction", SubFunction, "SubFunction");
+
+            initialized = true;
+        }
+    }
+}
