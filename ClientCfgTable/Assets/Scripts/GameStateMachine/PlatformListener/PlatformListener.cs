eb0b22799a2e90cc4817b3bae69590944672f173
liyangwei999 <1611086176@qq.com>
2019-07-21 19:01:04 +0800

    新增PlatformListener.cs

---
+++ ClientCfgTable/Assets/Scripts/GameStateMachine/PlatformListener/PlatformListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using LywGames;
+
+public class PlatformListener : AutoCreateSingleton<PlatformListener>, IPlatformListener
+{
+    public void OnIntiailized()
+    {
+        
+    }
+
+    public void OnLoginResult(bool success, string userId, string username, string channelUserId, string token, string productCode, string channelLabel, string channelId, int kodChannelId)
+    {
+        
+    }
+
+    public void OnLogout()
+    {
+        
+    }
+
+    public void OnNoPlatformLoginProvide()
+    {
+        
+    }
+}
