806ad1f4e9646b8df17975217266766032b9e5c2
liyangwei999 <1611086176@qq.com>
2019-07-21 19:40:19 +0800

    新增AssetBundleManager.cs

---
+++ ClientCfgTable/Assets/Scripts/Managers/AssetBundleManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AssetBundleManager : AbsManager<AssetBundleManager>
+{
+    public static UnityEngine.Object LoadAssetSync(string assetBundleName, string assetName = "")
+    {
+        return null;
+    }
+
+    public static void UnloadAssetBundle(string assetBundleName)
+    {
+
+    }
+
+}
+
