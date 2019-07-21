504838705abe3511f1e976a5e2cf650d24a47b50
liyangwei999 <1611086176@qq.com>
2019-07-21 19:39:58 +0800

    新增AssetPathUtility.cs

---
+++ ClientCfgTable/Assets/Scripts/ClientCommon/Utility/AssetPathUtility.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using LywGames;
+
+namespace ClientCommon
+{
+	public static class AssetPathUtility
+	{
+		static Dictionary<int, string> assetPathDic = new Dictionary<int, string>();
+
+		public static string GetTypePath(int assetType)
+		{
+			string assetPath = string.Empty;
+			if (!assetPathDic.TryGetValue(assetType, out assetPath))
+			{
+				var assetPathCfg = ConfigDataBase.AssetPathConfig.Get(assetType);
+                if (assetPathCfg == null)
+                {
+                    return assetPath;
+                }
+				assetPath = assetPathCfg.Path;
+				assetPathDic.Add(assetType, assetPath);
+			}
+
+			return assetPath;
+		}
+
+		public static string GetAssetPath(int assetType, string assetName)
+		{
+			return PathUtility.Combine(GetTypePath(assetType), assetName);
+		}
+
+	}
+}
