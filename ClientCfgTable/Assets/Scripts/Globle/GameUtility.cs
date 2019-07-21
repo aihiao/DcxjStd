fe2cddd3ea0c5fdffe402df8fda6804ae83fdfec
liyangwei999 <1611086176@qq.com>
2019-07-21 19:00:02 +0800

    GetUiString

---
+++ ClientCfgTable/Assets/Scripts/Globle/GameUtility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using ClientCommon;
+
+public static class GameUtility
+{
+    public static string GetUiString(string key)
+    {
+        var value = ConfigDataBase.TextConfig.Get(key);
+        if (value != null)
+        {
+            return value.Content;
+        }
+        return "Can't find string for key:" + key;
+    }
+}
