c2eb946cc4ce54b264c3b90313b92a5c8d467e73
liyangwei999 <1611086176@qq.com>
2019-07-21 10:28:44 +0800

    back键返回ui

--- ClientCfgTable/Assets/Scripts/Managers/UiManager/UiManager.cs
+++ ClientCfgTable/Assets/Scripts/Managers/UiManager/UiManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using ClientCommon;
 
 /// <summary>
 /// 1. 管理Ui创建销毁
@@ -13,7 +14,7 @@ public class UiManager : AbsManager<UiManager>
 
     private List<Type> firstUiList; // 记录固定显示的界面, 不添加到显示列表
 
-    #region 显示的ui
+    #region 所有显示的ui
     // 显示ui的容器
     private Dictionary<Type, BaseUi> showingUiDic = new Dictionary<Type, BaseUi>();
     private List<Type> showingList = new List<Type>();
@@ -110,17 +111,7 @@ public class UiManager : AbsManager<UiManager>
             Hide(firstUiList[i]);
         }
     }
-    #endregion 显示的ui
-
-    public T SetIsShowing<T>(bool isShowing) where T : BaseUi
-    {
-        if (isShowing)
-        {
-            Hide<T>();
-            return Show<T>();
-        }
-        return Hide<T>();
-    }
+    #endregion 所有显示的ui
 
     /// <summary>
     /// 通过名字显示ui
@@ -140,7 +131,7 @@ public class UiManager : AbsManager<UiManager>
         }
         else
         {
-            // 在UiCityPreloadManager预加载的需要进行这一步
+            // 在UiCityPreloadManager预加载的ui需要进行这一步
             ui.UiPanelName = typeName;
             InitUi(ui);
         }
@@ -148,7 +139,7 @@ public class UiManager : AbsManager<UiManager>
         // 若没有cache, 根据名字load对应的prefab
         if (ui == null)
         {
-            ui = LoadUiPerfabByName(typeName);
+            ui = LoadUiPrefabByName(typeName);
             ui.UiPanelName = typeName;
             InitUi(ui);
         }
@@ -161,11 +152,10 @@ public class UiManager : AbsManager<UiManager>
     /// </summary>
     /// <param name="typeName"></param>
     /// <returns></returns>
-    private BaseUi LoadUiPerfabByName(string typeName)
+    private BaseUi LoadUiPrefabByName(string typeName)
     {
         GameObject go = GameObjectUtility.CreateGameObject(typeName);
         go.SetActive(false);
-        Type type = Type.GetType(typeName);
         Type t = Type.GetType(typeName, false, true);
         var cmp = go.AddComponent(t);
 
@@ -175,6 +165,80 @@ public class UiManager : AbsManager<UiManager>
             cmpUi.BlandGameObjectLinkIfDidnt();
         }
         return cmpUi;
+    }
+
+    /// <summary>
+    /// ui prefab被创建后, 初始化
+    /// </summary>
+    /// <param name="ui"></param>
+    private void InitUi(BaseUi ui)
+    {
+        if (ui != null)
+        {
+            UiRelations.Instance.AddUi(ui);
+            ui.OnShow = uiShell.OnShow;
+            ui.OnHide = uiShell.OnHide;
+            ui.OnDestroyUi = OnDestroyUi;
+            ui.RunInitialize();
+        }
+    }
+
+    /// <summary>
+    /// ui显示方法, 调用BaseUi的显示方法
+    /// </summary>
+    /// <param name="ui"></param>
+    /// <param name="layer"></param>
+    /// <param name="dataList"></param>
+    /// <returns></returns>
+    private BaseUi Show(BaseUi ui, UiLayer layer = UiLayer.Normal, params object[] dataList)
+    {
+        HideMutex(ui.GetType(), layer);
+        if (ui != null)
+        {
+            if (layer != UiLayer.Normal)
+            {
+                ui.layer = layer;
+            }
+            ui.Show(dataList);
+        }
+        return ui;
+    }
+
+    /// <summary>
+    /// 这个创建ui, prefab本身已经绑定了脚本, 但是也没有调用BaseUi的BlandGameObjectLinkIfDidnt方法啊???
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    private BaseUi Create(Type type)
+    {
+        UiRelationData relation = UiRelations.Instance.GetUiRelationData(type);
+        if (relation == null)
+        {
+            LoggerManager.Instance.Error("Create ui type: {0} cannot find.", type);
+            return null;
+        }
+        BaseUi ui = UiUtility.LoadUiPerfab(relation.type, relation.resourceName);
+        if (ui != null)
+        {
+            ui.UiPanelName = relation.resourceName;
+            InitUi(ui);
+        }
+        return ui;
+    }
+
+    public BaseUi GetUiCreateWhenNotFind(Type type)
+    {
+        BaseUi ui = UiCityPreloadManager.Instance.GetCacheUi(type);
+        if (ui != null)
+        {
+            return ui;
+        }
+        ui = UiRelations.Instance.GetUi(type);
+        if (ui == null)
+        {
+            return Create(type);
+        }
+        return ui;
     }
 
     public T Show<T>(params object[] dataList) where T : BaseUi
@@ -200,56 +264,60 @@ public class UiManager : AbsManager<UiManager>
         return null;
     }
 
-    public T Hide<T>() where T : BaseUi
+    /// <summary>
+    /// 显示Ui, 以及关联Ui
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="layer"></param>
+    /// <param name="dataList"></param>
+    /// <returns></returns>
+    private BaseUi Show(Type type, UiLayer layer = UiLayer.Normal, params object[] dataList)
     {
-        return (T)Hide(typeof(T));
+        HideMutex(type, layer);
+
+        List<Type> linkedList = UiRelations.Instance.GetLinkedList(type);
+        if (linkedList == null || linkedList.Count <= 0)
+        {
+            return null;
+        }
+        BaseUi mainUi = null;
+        Type tempType = linkedList[0];
+        linkedList.RemoveAt(0);
+        linkedList.Add(tempType);
+        for (int i = 0; i < linkedList.Count; i++)
+        {
+            BaseUi ui = GetUiCreateWhenNotFind(linkedList[i]);
+            if (ui != null)
+            {
+                if (ui.GetType() == type)
+                {
+                    if (layer != UiLayer.Normal)
+                    {
+                        ui.layer = layer;
+                    }
+                    mainUi = ui;
+                }
+                ui.Show(dataList);
+            }
+        }
+        return mainUi;
     }
 
-    public BaseUi Hide(string typeName)
+    public T SetIsShowing<T>(bool isShowing) where T : BaseUi
     {
-        Type t = Type.GetType(typeName, false, true);
-        if (t != null)
+        if (isShowing)
         {
-            return Hide(t);
+            Hide<T>();
+            return Show<T>();
         }
-        LoggerManager.Instance.Error("UiManager Hide method cannot get type: {0} from typeName.", typeName);
-        return null;
+        return Hide<T>();
     }
 
-    public BaseUi Hide(Type type)
-    {
-        BaseUi ui = UiRelations.Instance.GetUi(type);
-        if (ui != null)
-        {
-            ui.Hide();
-        }
-        return ui;
-    }
-
-    public T GetUi<T>(bool createIfNotFind = false) where T : BaseUi
-    {
-        if (createIfNotFind)
-        {
-            return (T)GetUiCreateWhenNotFind(typeof(T));
-        }
-        return (T)UiRelations.Instance.GetUi(typeof(T));
-    }
-
-    public BaseUi GetUiCreateWhenNotFind(Type type)
-    {
-        BaseUi ui = UiCityPreloadManager.Instance.GetCacheUi(type);
-        if (ui != null)
-        {
-            return ui;
-        }
-        ui = UiRelations.Instance.GetUi(type);
-        if (ui == null)
-        {
-            return Create(type);
-        }
-        return ui;
-    }
-
+    /// <summary>
+    /// 创建ui, 虽然没有调用InitUi方法, 但是在Show的时候会调用
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
     public BaseUi CreateUiNotShow(Type type)
     {
         GameObject go = GameObjectUtility.CreateGameObject(type.Name);
@@ -263,9 +331,13 @@ public class UiManager : AbsManager<UiManager>
         return cmpUi;
     }
 
-    public bool GetIsShowing<T>()
+    public T GetUi<T>(bool createIfNotFind = false) where T : BaseUi
     {
-        return UiRelations.Instance.GetIsShowing(typeof(T));
+        if (createIfNotFind)
+        {
+            return (T)GetUiCreateWhenNotFind(typeof(T));
+        }
+        return (T)UiRelations.Instance.GetUi(typeof(T));
     }
 
     public BaseUi GetTopestShowingUi(Func<BaseUi, bool> addationCheckCondation = null)
@@ -334,48 +406,46 @@ public class UiManager : AbsManager<UiManager>
         return currentSelected;
     }
 
-    public void DestroyAll(bool iscludeShowings = false)
+    public bool GetIsShowing<T>()
     {
-        List<BaseUi> toBeRemoveUis = new List<BaseUi>();
-        List<string> removeds = new List<string>();
-        foreach (var uiPair in UiRelations.Instance.UiDic)
-        {
-            if (uiPair.Value.ignoreDestory)
-            {
-                continue;
-            }
-            if (uiPair.Value.MarkedNotDestroyWhenDestroyAll)
-            {
-                uiPair.Value.MarkedNotDestroyWhenDestroyAll = false;
-            }
-            else
-            {
-                toBeRemoveUis.Add(uiPair.Value);
-                removeds.Add(uiPair.Key);
-            }
-        }
-
-        for (int i = 0; i < removeds.Count; i++)
-        {
-            if (UiRelations.Instance.UiDic.ContainsKey(removeds[i]))
-            {
-                UiRelations.Instance.UiDic.Remove(removeds[i]);
-            }
-        }
-
-        foreach (var baseUi in toBeRemoveUis)
-        {
-            if (baseUi != null)
-            {
-                if (baseUi.gameObject.activeSelf)
-                {
-                    baseUi.Hide();
-                }
-                baseUi.Destroy();
-            }
-        }
+        return UiRelations.Instance.GetIsShowing(typeof(T));
     }
 
+    public T Hide<T>() where T : BaseUi
+    {
+        return (T)Hide(typeof(T));
+    }
+
+    public BaseUi Hide(string typeName)
+    {
+        Type t = Type.GetType(typeName, false, true);
+        if (t != null)
+        {
+            return Hide(t);
+        }
+        LoggerManager.Instance.Error("UiManager Hide method cannot get type: {0} from typeName.", typeName);
+        return null;
+    }
+
+    public BaseUi Hide(Type type)
+    {
+        BaseUi ui = UiRelations.Instance.GetUi(type);
+        if (ui != null)
+        {
+            ui.Hide();
+        }
+        return ui;
+    }
+
+    private void HideMutex(Type type, UiLayer layer)
+    {
+
+    }
+
+    /// <summary>
+    /// 关闭所有显示的ui
+    /// </summary>
+    /// <param name="layers"></param>
     public void CloseAllUiByUiLayer(params UiLayer[] layers)
     {
         for (int i = 0; i < layers.Length; i++)
@@ -432,6 +502,52 @@ public class UiManager : AbsManager<UiManager>
         }
     }
 
+    /// <summary>
+    /// 销毁所有ui
+    /// </summary>
+    /// <param name="iscludeShowings"></param>
+    public void DestroyAll(bool iscludeShowings = false)
+    {
+        List<BaseUi> toBeRemoveUis = new List<BaseUi>();
+        List<string> removeds = new List<string>();
+        foreach (var uiPair in UiRelations.Instance.UiDic)
+        {
+            if (uiPair.Value.ignoreDestory)
+            {
+                continue;
+            }
+            if (uiPair.Value.MarkedNotDestroyWhenDestroyAll)
+            {
+                uiPair.Value.MarkedNotDestroyWhenDestroyAll = false;
+            }
+            else
+            {
+                toBeRemoveUis.Add(uiPair.Value);
+                removeds.Add(uiPair.Key);
+            }
+        }
+
+        for (int i = 0; i < removeds.Count; i++)
+        {
+            if (UiRelations.Instance.UiDic.ContainsKey(removeds[i]))
+            {
+                UiRelations.Instance.UiDic.Remove(removeds[i]);
+            }
+        }
+
+        foreach (var baseUi in toBeRemoveUis)
+        {
+            if (baseUi != null)
+            {
+                if (baseUi.gameObject.activeSelf)
+                {
+                    baseUi.Hide();
+                }
+                baseUi.Destroy();
+            }
+        }
+    }
+
     public static bool IsPosInsideScreen(Vector3 screenPos)
     {
         int offset = 1;
@@ -440,74 +556,6 @@ public class UiManager : AbsManager<UiManager>
             return false;
         }
         return true;
-    }
-
-    private void HideMutex(Type type, UiLayer layer)
-    {
-
-    }
-
-    private BaseUi Create(Type type)
-    {
-        UiRelationData relation = UiRelations.Instance.GetUiRelationData(type);
-        if (relation == null)
-        {
-            LoggerManager.Instance.Error("Create ui type: {0} cannot find.", type);
-            return null;
-        }
-        BaseUi ui = UiUtility.LoadUiPerfab(relation.type, relation.resourceName);
-        if (ui != null)
-        {
-            ui.UiPanelName = relation.resourceName;
-            InitUi(ui);
-        }
-        return ui;
-    }
-
-    private BaseUi Show(Type type, UiLayer layer = UiLayer.Normal, params object[] dataList)
-    {
-        HideMutex(type, layer);
-
-        List<Type> linkedList = UiRelations.Instance.GetLinkedList(type);
-        if (linkedList == null || linkedList.Count <= 0)
-        {
-            return null;
-        }
-        BaseUi mainUi = null;
-        Type tempType = linkedList[0];
-        linkedList.RemoveAt(0);
-        linkedList.Add(tempType);
-        for (int i = 0; i < linkedList.Count; i++)
-        {
-            BaseUi ui = GetUiCreateWhenNotFind(linkedList[i]);
-            if (ui != null)
-            {
-                if (ui.GetType() == type)
-                {
-                    if (layer != UiLayer.Normal)
-                    {
-                        ui.layer = layer;
-                    }
-                    mainUi = ui;
-                }
-                ui.Show(dataList);
-            }
-        }
-        return mainUi;
-    }
-
-    private BaseUi Show(BaseUi ui, UiLayer layer = UiLayer.Normal, params object[] dataList)
-    {
-        HideMutex(ui.GetType(), layer);
-        if (ui != null)
-        {
-            if (layer != UiLayer.Normal)
-            {
-                ui.layer = layer;
-            }
-            ui.Show(dataList);
-        }
-        return ui;
     }
 
     #region 框架方法
@@ -526,6 +574,34 @@ public class UiManager : AbsManager<UiManager>
     public override void OnUpdate()
     {
         base.OnUpdate();
+
+        if (Application.platform == RuntimePlatform.Android && Input.GetKeyUp(KeyCode.Escape))
+        {
+            bool guideShowing = (UiManager.Instance != null && UiManager.Instance.GetUi<UiPnlGuide>() != null && UiManager.Instance.GetUi<UiPnlGuide>().IsShowing);
+            if (!guideShowing)
+            {
+                var emulator = UiRelations.Instance.UiDic.GetEnumerator();
+                var topMostUi = GetTopestShowingUi((BaseUi ui) =>
+                {
+                    var uiCfg = ConfigDataBase.UiPannelConfig.Get(ui.UiPanelName);
+                    if (uiCfg != null && uiCfg.CanCloseByBackKey)
+                    {
+                        return true;
+                    }
+                    return false;
+                });
+
+                if (topMostUi != null)
+                {
+                    LoggerManager.Instance.Info("On escape, close : " + topMostUi.UiPanelName);
+                    topMostUi.OnReturn(topMostUi.gameObject);
+                }
+                else
+                {
+
+                }
+            }
+        }
     }
 
     public override void Dispose()
@@ -535,18 +611,10 @@ public class UiManager : AbsManager<UiManager>
     }
     #endregion 框架方法
 
-    private void InitUi(BaseUi ui)
-    {
-        if (ui != null)
-        {
-            UiRelations.Instance.AddUi(ui);
-            ui.OnShow = uiShell.OnShow;
-            ui.OnHide = uiShell.OnHide;
-            ui.OnDestroyUi = OnDestroyUi;
-            ui.RunInitialize();
-        }
-    }
-
+    /// <summary>
+    /// 销毁ui, 被BaseUi反向调用
+    /// </summary>
+    /// <param name="ui"></param>
     public void OnDestroyUi(BaseUi ui)
     {
         if (ui != null)
