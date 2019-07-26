c6a2c2598af55ea43db6d6e0f54463b18659853a
liyangwei999 <1611086176@qq.com>
2019-07-26 23:15:14 +0800

    新增SignalGenerator.cs

---
+++ ClientCfgTable/Assets/Scripts/Combat/Signal/SignalGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Combat
+{
+    public class SignalGenerator
+    {
+        private object parent = null;
+        private Dictionary<int, List<ISignalListener>> signalListenerDic;
+        private bool listModified = false;
+        private int recurseLevel = 0;
+
+        public SignalGenerator() { }
+        public SignalGenerator(object parent)
+        {
+            this.parent = parent;
+        }
+
+    }
+}
