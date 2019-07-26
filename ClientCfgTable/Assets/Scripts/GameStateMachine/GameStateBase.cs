8747cf3641ad57b3c908d6b5d4853ef9b3f7b4b0
liyangwei999 <1611086176@qq.com>
2019-07-26 20:01:56 +0800

    StartLoadingBarUpdateCoroutine

--- ClientCfgTable/Assets/Scripts/GameStateMachine/GameStateBase.cs
+++ ClientCfgTable/Assets/Scripts/GameStateMachine/GameStateBase.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Reflection;
+using UnityEngine;
+using Combat;
 
 /// <summary>
 /// 游戏状态机基类
@@ -70,6 +73,42 @@ public abstract class GameStateBase : MonoBehaviour
     public bool IsCombatState()
     {
         return StateType == GameStateType.Dungeon;
+    }
+
+    protected void StartLoadingBarUpdateCoroutine(UiPnlLoading loading)
+    {
+        StopCoroutine("UpdateLoadingBar");
+        StartCoroutine(UpdateLoadingBar(loading));
+    }
+
+    [Obfuscation(Exclude = true, Feature = "renaming")]
+    protected IEnumerator UpdateLoadingBar(UiPnlLoading loading)
+    {
+        int safeGuard = 0;
+        loading.SetSliderValue(0f);
+        while (loading.loadingProgress.CurrentAsyncLoadingProgress < 1f)
+        {
+            loading.SetSliderValue(loading.loadingProgress.CurrentAsyncLoadingProgress);
+            safeGuard++;
+            if (safeGuard >= 25 * 1000 / 33)
+            {
+                break;
+            }
+            yield return null;
+        }
+        loading.SetSliderValue(1f);
+        yield return null; 
+    }
+
+    protected ObjectBase CreateObject(int templateId, int idForGenerateName, bool clientNotServer, int createionMethod = ObjectCreationContext.CreationMethodType.OBJECT_CREATION_METHOD)
+    {
+        ObjectCreationContext context = new ObjectCreationContext();
+        context.templateId = templateId;
+        context.clientNotServer = clientNotServer;
+        context.CreationMethod = createionMethod;
+        context.NetworkID = idForGenerateName;
+
+        return InstanceBase.GetCurrentInstance().GetObjectManager().CreateObject(context);
     }
 
 }
