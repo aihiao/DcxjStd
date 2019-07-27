67bcf9b1dfb4b83aac28f5dae6c1bbee9eef926a
liyangwei999 <1611086176@qq.com>
2019-07-27 17:06:14 +0800

    新增ObjectManager.cs

---
+++ ClientCfgTable/Assets/Scripts/Combat/ObjectManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Combat
+{
+    public class ObjectManager
+    {
+        public virtual ObjectBase CreateObject(ObjectCreationContext context)
+        {
+            return null;
+        }
+    }
+}
