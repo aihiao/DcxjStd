b204e4a9a4028c3bbfa1316726ed6405498434a5
liyangwei999 <1611086176@qq.com>
2019-07-27 18:17:28 +0800

    NetworkParameters

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/NetworkParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace LywGames.Messages
+{
+    public class NetworkParameters
+    {
+        public static int _MAX_RECV_BUFFER_SIZE = 102400;
+        public static int _MAX_SEND_BUFFER_SIZE = 2048;
+        public static int _MAX_COMPRESS_MESSAGE_SIZE = 196611;
+        public static int _MAX_UNCOMPRESS_MESSAGE_SIZE = 1048576;
+    }
+}
