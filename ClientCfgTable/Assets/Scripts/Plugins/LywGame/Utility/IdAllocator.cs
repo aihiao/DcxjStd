79ab3dbfd0fb6a2b7d5e8ed9ac88f87ea4721d3c
liyangwei999 <1611086176@qq.com>
2019-07-27 18:35:19 +0800

    IdAllocator

---
+++ ClientCfgTable/Assets/Scripts/Plugins/LywGame/Utility/IdAllocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LywGames
+{
+    public class IdAllocator
+    {
+        public int NewId()
+        {
+            do
+            {
+                idCounter++;
+
+                if (idCounter >= Int32.MaxValue)
+                {
+                    idCounter = idStart;
+                    idLoops++;
+                }
+
+            } while (idLoops > 0 && usedIds.Contains(idCounter));
+
+            usedIds.Add(idCounter);
+
+            return idCounter;
+        }
+
+        public void ReleaseID(int id)
+        {
+            if (id == InvalidId)
+                return;
+
+            usedIds.Remove(id);
+        }
+
+        public const int InvalidId = 0;
+        protected const int idStart = InvalidId + 1;
+        protected int idCounter = idStart;
+        protected int idLoops = 0;
+        protected List<int> usedIds = new List<int>();
+    }
+}
