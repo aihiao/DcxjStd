5575205b819d32f8881721e718b73ca4aa64aacd
liyangwei999 <1611086176@qq.com>
2019-07-27 18:18:01 +0800

    TimeInfo

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Message/Game/TimeInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using ProtoBuf;
+
+namespace LywGames.Message.Proto.Game
+{
+    [ProtoContract(Name = "TimeInfo")]
+    public class TimeInfo : IExtensible
+    {
+        private long _millsecond = 0L;
+        private long _timezone = 0L;
+        private IExtension extensionObject;
+
+        [ProtoMember(0), DefaultValue(0L)]
+        public long millsecond
+        {
+            get
+            {
+                return this._millsecond;
+            }
+            set
+            {
+                this._millsecond = value;
+            }
+        }
+
+        [ProtoMember(0), DefaultValue(0L)]
+        public long timezone
+        {
+            get
+            {
+                return this._timezone;
+            }
+            set
+            {
+                this._timezone = value;
+            }
+        }
+
+        IExtension IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return Extensible.GetExtensionObject(ref this.extensionObject, createIfMissing);
+        }
+
+    }
+}
