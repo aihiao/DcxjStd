0f41bc1bd843f7cac0150064069290a40833adde
liyangwei999 <1611086176@qq.com>
2019-07-27 18:17:04 +0800

    MessageAttribute

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/MessageAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LywGames.Messages
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, Inherited = true)]
+    public class MessageAttribute : Attribute
+    {
+        private int protocolId;
+        private bool isRequest;
+        public int ProtocolId
+        {
+            get
+            {
+                return this.protocolId;
+            }
+            set
+            {
+                this.protocolId = value;
+            }
+        }
+
+        public bool IsRequest
+        {
+            get
+            {
+                return this.isRequest;
+            }
+            set
+            {
+                this.isRequest = value;
+            }
+        }
+
+    }
+}
