dfed5d00c23777a6a469890cf4dfc5de761937c0
liyangwei999 <1611086176@qq.com>
2019-07-27 17:01:42 +0800

    新增ObjectBase.cs

---
+++ ClientCfgTable/Assets/Scripts/Combat/ObjectBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Combat
+{
+    public class ObjectBase : ISignalGenerator
+    {
+        public SignalGenerator GetSignalGenerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
