c59dce5d731f5d8c6ad17cb7f5d6f7cbb72c9d03
liyangwei999 <1611086176@qq.com>
2019-07-27 17:36:38 +0800

    新增InstanceBase.cs

---
+++ ClientCfgTable/Assets/Scripts/Combat/Instance/InstanceBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Combat
+{
+    abstract class InstanceBase : ISignalListener
+    {
+        protected ObjectManager objectManager = null;
+
+        public void ReceiveSignal(SignalGenerator generator, int signalType, SignalData signalData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static InstanceBase GetCurrentInstance()
+        {
+            return null;
+        }
+
+        public ObjectManager GetObjectManager() { return objectManager; }
+
+    }
+}
