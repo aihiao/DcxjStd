40cb8b974dbe628dcaab928ad0274fc171ca021e
liyangwei999 <1611086176@qq.com>
2019-07-27 17:36:21 +0800

    战斗中的游戏实例, 一般战斗单元都是用这个类, 支持客户端服务器通信机制

---
+++ ClientCfgTable/Assets/Scripts/Combat/GameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LywGames;
+
+namespace Combat
+{
+    /// <summary>
+	/// 战斗中的游戏实例, 一般战斗单元都是用这个类, 支持客户端服务器通信机制
+	/// </summary>
+    public class GameObject : ObjectBase
+    {
+    }
+}
