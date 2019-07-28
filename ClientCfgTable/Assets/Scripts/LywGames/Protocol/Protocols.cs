aa7af65aa0c22d9c6451bf84e4c4a819cbe9013a
liyangwei999 <1611086176@qq.com>
2019-07-28 09:32:39 +0800

    Protocols isSuccess方法

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Protocols.cs
@@ -0,0 +1,10 @@
+﻿namespace LywGames.Corgi.Protocol
+{
+    public class Protocols 
+    {
+        public static bool isSuccess(int id)
+        {
+            return id <= 0 || ((long)id & (long)(-1048576)) >> 20 < 16L;
+        }
+    }
+}
