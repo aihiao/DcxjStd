a467a0ac8c08b9b7f0d669ee32ca3578370a608d
liyangwei999 <1611086176@qq.com>
2019-07-28 09:41:54 +0800

    获得设备网络状态

--- ClientCfgTable/Assets/Scripts/PluginRenewable/ExternalCall/LywConfig.cs
+++ ClientCfgTable/Assets/Scripts/PluginRenewable/ExternalCall/LywConfig.cs
@@ -24,19 +24,21 @@ namespace LywGames
         private static extern IntPtr UnityCall_LywConfig_GetServerIP();
         [DllImport("__Internal")]
         private static extern int UnityCall_LywConfig_GetServerPort();
+        [DllImport("__Internal")]
+        private static extern int UnityCall_LywConfig_GetDeviceConnectedInternetStatus();
 
         private static AndroidJavaClass javaCls = null;
         private static AndroidJavaClass GetJavaCls()
         {
-            return javaCls == null ? javaCls = new AndroidJavaClass("") : javaCls;
+            return javaCls == null ? javaCls = new AndroidJavaClass("com.lywgames.LywConfig") : javaCls;
         }
 
         public static int GetPublisher()
         {
 #if UNITY_IPHONE
-            UnityCall_LywConfig_GetPublisher();
+            return UnityCall_LywConfig_GetPublisher();
 #elif UNITY_ANDROID
-            GetJavaCls().CallStatic<int>("getPublisher");
+            return GetJavaCls().CallStatic<int>("getPublisher");
 #else
             return ProductPublisher.Local;
 #endif
@@ -45,9 +47,9 @@ namespace LywGames
         public static string GetServerIP()
         {
 #if UNITY_IPHONE
-            Marshal.PtrToStringAnsi(UnityCall_LywConfig_GetServerIP());
+            return Marshal.PtrToStringAnsi(UnityCall_LywConfig_GetServerIP());
 #elif UNITY_ANDROID
-            GetJavaCls().CallStatic<string>("getServerIP");
+            return GetJavaCls().CallStatic<string>("getServerIP");
 #else
             return "127.0.0.1";
 #endif
@@ -56,9 +58,9 @@ namespace LywGames
         public static int GetServerPort()
         {
 #if UNITY_IPHONE
-            UnityCall_LywConfig_GetServerPort();
+            return UnityCall_LywConfig_GetServerPort();
 #elif UNITY_ANDROID
-            GetJavaCls().CallStatic<int>("getServerPort");
+            return GetJavaCls().CallStatic<int>("getServerPort");
 #else
             return 3771;
 #endif
@@ -68,6 +70,23 @@ namespace LywGames
         {
 #if UNITY_ANDROID
 			GetJavaClass().CallStatic("show", title, message, okText, cancelText);
+#endif
+        }
+
+        /// <summary>
+		/// 获得设备网络状态
+		/// 0 未联网
+		/// 1 wifi联网
+		/// 2 移动网络
+		/// </summary>
+		public static int GetDeviceConnectedInternetStatus()
+        {
+#if UNITY_IPHONE
+            return UnityCall_LywConfig_GetDeviceConnectedInternetStatus();
+#elif UNITY_ANDROID
+            return GetJavaCls().CallStatic<int>("getDeviceConnectedInternetStatus");
+#else
+            return 0;
 #endif
         }
 
