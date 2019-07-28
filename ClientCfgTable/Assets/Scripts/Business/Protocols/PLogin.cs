33ad0613ff1cc3b194bde0f7fb64d586979dbc75
liyangwei999 <1611086176@qq.com>
2019-07-28 09:43:19 +0800

    新增PLogin.cs

---
+++ ClientCfgTable/Assets/Scripts/Business/Protocols/PLogin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LywGames.Messages;
+
+/// <summary>
+/// 链接Gs
+/// </summary>
+class PLoginGs
+{
+}
+
+/// <summary>
+/// 请求玩家数据协议，对应网络连接状态为 NETSTATUS_QUERYDATA
+/// </summary>
+public class PQueryLoginGameData : AbsRequest<CGQueryLoginGameMessage>
+{
+    private long roleId = long.MaxValue;
+    public PQueryLoginGameData(long roleId)
+    {
+        this.roleId = roleId;
+    }
+}
