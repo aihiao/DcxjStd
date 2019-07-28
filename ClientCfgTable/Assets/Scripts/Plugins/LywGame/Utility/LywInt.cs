853d29ad39734f4fc19caefa2b424f060ce1a905
liyangwei999 <1611086176@qq.com>
2019-07-28 10:33:08 +0800

    LywInt

---
+++ ClientCfgTable/Assets/Scripts/Plugins/LywGame/Utility/LywInt.cs
@@ -0,0 +1,39 @@
+﻿public struct LywInt
+{
+    static int key = 38796541;
+    int mValue;
+
+    public static void SetKey(int genKey)
+    {
+        key = genKey;
+    }
+
+#if UNITY_EDITOR
+    int debugValue;
+    int initedFlag;// = false;
+#endif
+
+    LywInt(int v)
+    {
+        mValue = v ^ key;
+#if UNITY_EDITOR
+        debugValue = v;
+        initedFlag = int.MaxValue;
+#endif
+    }
+
+    public static implicit operator int(LywInt v)
+    {
+        int res = v.mValue ^ key;
+#if UNITY_EDITOR
+        if (v.debugValue != res && v.initedFlag == int.MaxValue)
+            LoggerManager.Instance.Error(string.Format("value incurrect,  orignal:{0}  mixed:{1}  res:{2} key:{3}", v.debugValue, v.mValue, res, key));
+#endif
+        return res;
+    }
+
+    public static implicit operator LywInt(int v)
+    {
+        return new LywInt(v);
+    }
+}
