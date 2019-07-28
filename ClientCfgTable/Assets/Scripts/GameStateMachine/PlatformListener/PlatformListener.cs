e7cb70dce2a4094a7328af4939dfd09ec332b6b1
liyangwei999 <1611086176@qq.com>
2019-07-28 10:32:38 +0800

    gameplay需要的外部接口

--- ClientCfgTable/Assets/Scripts/GameStateMachine/PlatformListener/PlatformListener.cs
+++ ClientCfgTable/Assets/Scripts/GameStateMachine/PlatformListener/PlatformListener.cs
@@ -28,4 +28,26 @@ public class PlatformListener : AutoCreateSingleton<PlatformListener>, IPlatform
 
     }
 
+    #region gameplay需要的外部接口
+    bool isPlatformInited = false;
+
+    bool isPlatformLoginProvide = true;
+    public bool IsPlatformLoginProvide { get { return isPlatformLoginProvide; } }
+
+    public bool IsWaitingForPlatformInitialize()
+    {
+        return !isPlatformInited;
+    }
+
+    // 显示游戏的登录界面, 支持自定义的登录界面
+    public void ShowLoginUIModule()
+    {
+        UiManager.Instance.ShowByName(UiPrefabNames.UiPnlLogin);
+    }
+
+    public void HideLoginUIModule()
+    {
+        UiManager.Instance.Hide<UiPnlLogin>();
+    }
+    #endregion
 }
