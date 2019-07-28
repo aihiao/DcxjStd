f787ac44cf0c1f5de0993bccaf0e85db5b20e0f2
liyangwei999 <1611086176@qq.com>
2019-07-28 10:32:59 +0800

    UiRegister

---
+++ ClientCfgTable/Assets/Scripts/Constants/UiRegister.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class UiRegister
+{
+    public static UiConfigData[] GetAllUiConfigDatas()
+    {
+        UiConfigData[] datas =
+        {
+        };
+
+        return datas;
+    }
+
+    public struct UiConfigData
+    {
+        public Type type;
+        public string prefabName;
+        public Type[] linkedTypes;
+        public bool hideOtherModules;
+        public Type[] ignoreMutexTypes;
+
+        public UiConfigData(Type type, string prefabName) : this(type, prefabName, null, false, null) { }
+        public UiConfigData(Type type, string prefabName, Type[] linkedTypes) : this(type, prefabName, linkedTypes, false, null) { }
+        public UiConfigData(Type type, string prefabName, Type[] linkedTypes, bool hideOtherModules, Type[] ignoreMutexTypes)
+        {
+            this.type = type;
+            this.prefabName = prefabName;
+            this.linkedTypes = linkedTypes;
+            this.hideOtherModules = hideOtherModules;
+            this.ignoreMutexTypes = ignoreMutexTypes;
+        }
+    }
+
+}
+
