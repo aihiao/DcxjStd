3dda164d9db06d73ecd911656f4663839f012c7f
liyangwei999 <1611086176@qq.com>
2019-07-28 10:33:20 +0800

    登录界面

---
+++ ClientCfgTable/Assets/Scripts/View/Views/Ui/UiPnlLogin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 登录界面
+/// </summary>
+public class UiPnlLogin : BaseUi
+{
+    private UIInput inputName;
+    private UIInput inputPassWord;
+    private UIButton loginBtn;
+    private UIButton registerBtn;
+    private GameObject backgroundObj;
+    private GameObject clickToStart;
+    private UIWidget fullScreenClick;
+    private UILabel versionText;
+}
