698cc51a839a2d2a373dd5196ade0279ad1b284b
liyangwei999 <1611086176@qq.com>
2019-07-29 20:55:15 +0800

    新增UiPnlGetMentalCheats.cs

---
+++ ClientCfgTable/Assets/Scripts/View/Views/Ui/TipsUi/UiPnlGetMentalCheats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 获得心法秘籍提示 
+/// 不和其他获得写在一处，是为了适应ShowQueue，这是个坑
+/// </summary>
+public class UiPnlGetMentalCheats : BaseUi
+{
+
+}
