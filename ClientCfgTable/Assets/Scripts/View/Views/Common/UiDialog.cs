6126c93c3ec470e06e91d539498b4f6d4dd677c8
liyangwei999 <1611086176@qq.com>
2019-07-29 20:57:02 +0800

    UiDialog btn

--- ClientCfgTable/Assets/Scripts/View/Views/Common/UiDialog.cs
+++ ClientCfgTable/Assets/Scripts/View/Views/Common/UiDialog.cs
@@ -13,6 +13,27 @@ public class UiDialogBtn
 
 public class UiDialog : BaseUi
 {
+    // TODO : 上次注释我说了YES和OK是同一样的意义, 如果对我写的注释有不同意见, 要跟我商量.
+    public UIButton YESBtn;
+    public static string yesLabel = "YES";
+
+    public UIButton OKBtn;
+    public static string okLabel = "OK";
+
+    public UIButton NOBtn;
+    public static string noLabel = "NO";
+
+    public UIButton CANCELBtn;
+    public static string cancelLabel = "CANCEL";
+
+    public UIButton CLOSEBtn;
+    public static string closeLabel = "CLOSE";
+
+    public UISprite innerPanel;
+    public UILabel title;
+    public UILabel content;
+    private object _data;
+
     public UiDialog Set(string contentText, uint btnFlag = UiDialogBtn.None, Action<AlertBtnType, object> callBack = null, bool destroyOnClick = true)
     {
         return null;
