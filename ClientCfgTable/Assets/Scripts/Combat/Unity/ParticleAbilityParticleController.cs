4d5e361b7d83f601a9ba487906babaa026cb47a1
liyangwei999 <1611086176@qq.com>
2019-07-31 18:33:45 +0800

    新增ParticleAbilityParticleController.cs

---
+++ ClientCfgTable/Assets/Scripts/Combat/Unity/ParticleAbilityParticleController.cs
@@ -0,0 +1,45 @@
+﻿namespace Combat
+{
+#if COMBAT_CLIENT
+	class ParticleAbilityParticleController : UnityEngine.MonoBehaviour
+	{
+		ParticleSystemSettings config;
+		//UnityEngine.GameObject particle;
+		public void Set(UnityEngine.GameObject particle, ParticleSystemSettings config, bool isPosition)
+		{
+			this.config = config;
+			//this.particle = particle;
+			initPosition = particle.transform.position;
+			initEuler = particle.transform.eulerAngles;
+			this.isPositionEffect = isPosition;
+		}
+
+		UnityEngine.Vector3 initPosition = default(UnityEngine.Vector3);
+		UnityEngine.Vector3 initEuler = default(UnityEngine.Vector3);
+		bool isPositionEffect = false;
+
+		void Update()
+		{
+			if (!isPositionEffect)
+				MangeTransform();
+		}
+
+		void LateUpdate()
+		{
+			if (!isPositionEffect)
+				MangeTransform();
+		}
+
+		void MangeTransform()
+		{
+			if (config.maintainPosition)
+				transform.position = initPosition;
+			else if (config.useLocalRelativePosition == false)
+				transform.position = transform.parent.position + config.placementWorldRelativePosition;
+
+			if (config.maintainFacing)
+				transform.eulerAngles = initEuler;
+		}
+	}
+#endif
+}
