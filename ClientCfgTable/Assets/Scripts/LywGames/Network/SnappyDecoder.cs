3749ec8a85ae773c62fa7bf531fea9deb096d277
liyangwei999 <1611086176@qq.com>
2019-08-03 20:11:32 +0800

    SnappyDecoder

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/SnappyDecoder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Snappy.Sharp;
+using LywGames.Messages;
+
+namespace LywGames.Network
+{
+    public class SnappyDecoder : AbstractNetworkInHandler
+    {
+        private SnappyDecompressor decompressor = new SnappyDecompressor();
+        public override void OnReceived(IConnection connection, byte[] buffer, int offset, int size)
+        {
+            byte b = buffer[offset];
+            if (b == Message.UNCOMPRESSED)
+            {
+                base.FireBuffReceived(connection, buffer, offset + 1, size - 1);
+            }
+            else
+            {
+                if (b == Message.COMPRESSED)
+                {
+                    byte[] array = this.decompressor.Decompress(buffer, offset + 1, size - 1);
+                    base.FireBuffReceived(connection, array, 0, array.Length);
+                }
+            }
+        }
+        public override void OnReceived(IConnection connection, object msg)
+        {
+            throw new NotSupportedException("SnappyFramedDecoder didn't implement OnReceived(IConnection connection, Object msg)");
+        }
+    }
+}
