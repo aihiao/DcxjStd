9efea729099edd75b5f160f2b329a0921f1280ba
liyangwei999 <1611086176@qq.com>
2019-08-03 10:37:50 +0800

    MySerializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/MySerializer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using LywGames.Common;
+using LywGames.Protocol;
+
+namespace LywGames.Messages
+{
+    public class MySerializer
+    {
+        private IProtobufSerializer protobufSerializer;
+
+        private static MySerializer mySerializer = new MySerializer();
+        public static int MAX_OUTMESSAGE_SIZE = 2048;
+        private MySerializer()
+        {
+        }
+        public static MySerializer GetInstance()
+        {
+            return mySerializer;
+        }
+
+        public void Initialize(bool useTypeMode)
+        {
+            if (useTypeMode)
+            {
+                this.protobufSerializer = new TypeModelProtobufSerializer(new Protocols_c());
+            }
+            else
+            {
+                this.protobufSerializer = new MetaDataProtobufSerializer();
+            }
+        }
+
+        public bool Serialize<T>(NetworkBuffer outBuffer, T instance)
+        {
+            outBuffer.GetStream().Position = (long)outBuffer.WriteOffset;
+            this.protobufSerializer.Serialize<T>(outBuffer.GetStream(), instance);
+            outBuffer.WriteOffset = (int)outBuffer.GetStream().Position;
+            return true;
+        }
+
+        public object DeserializeByteBuffer(byte[] buffer, int offset, int size, Type protoType)
+        {
+            MemoryStream source = new MemoryStream(buffer, offset, size);
+            object result = null;
+            try
+            {
+                result = this.protobufSerializer.Deserialize(source, protoType);
+            }
+            catch (Exception ex)
+            {
+                LoggerManager.Instance.Error(ex.StackTrace);
+            }
+            return result;
+        }
+
+    }
+}
