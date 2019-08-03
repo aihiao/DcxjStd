90959d2a3a4eb1c1e0b4208763b4b3b6994fd93b
liyangwei999 <1611086176@qq.com>
2019-08-03 20:00:18 +0800

    ClientHelperNetworkInitializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/ClientHelper/ClientHelperNetworkInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using LywGames.Protocol;
+using LywGames.Network;
+
+namespace LywGames.ClientHelper
+{
+    public class ClientHelperNetworkInitializer : AbstractNetworkInitializer
+    {
+        private AbstractMessageInitializer msgInitializer;
+        private MessageDelegateProcessor msgDelegateProcessor;
+        private ProtocolType type;
+        public ClientHelperNetworkInitializer(AbstractMessageInitializer msgInitializer, MessageDelegateProcessor msgDelegateProcessor, ProtocolType type)
+        {
+            this.msgInitializer = msgInitializer;
+            this.msgDelegateProcessor = msgDelegateProcessor;
+            this.type = type;
+        }
+        public override void Initial(NetworkHandlerPipeline pipeline, ConnectionType cnType)
+        {
+            if (this.type != ProtocolType.Udp || (cnType != ConnectionType.CONNECTION_GAME && cnType != ConnectionType.CONNECTION_BATTLE))
+            {
+                pipeline.AddHandler(new LengthFieldBasedFrameDecoder(204800, 0, 4, 0, 4));
+                pipeline.AddHandler(new LengthFieldPrepender(4, 0, false));
+            }
+            pipeline.AddHandler(new SnappyDecoder());
+            pipeline.AddHandler(new SnappyEncoder());
+            pipeline.AddHandler(new MessageInProcessor(this.msgInitializer, this.msgDelegateProcessor));
+            pipeline.AddHandler(new MessageOutProcessor());
+        }
+    }
+}
