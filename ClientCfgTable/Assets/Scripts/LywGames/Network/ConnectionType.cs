68934ab5d64b7d12e73caf749fa2efdf1227d7af
liyangwei999 <1611086176@qq.com>
2019-08-03 19:46:17 +0800

    ConnectionType

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/ConnectionType.cs
@@ -0,0 +1,10 @@
+﻿namespace LywGames.Network
+{
+    public enum ConnectionType
+    {
+        CONNECTION_UNKNOWN,
+        CONNECTION_AUTH,
+        CONNECTION_GAME,
+        CONNECTION_BATTLE
+    }
+}
