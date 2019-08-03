c21b52026b5bd5a268a3ae965636eaf26ea1ac93
liyangwei999 <1611086176@qq.com>
2019-08-03 10:37:43 +0800

    IProtobufSerializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/IProtobufSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace LywGames.Messages
+{
+    public interface IProtobufSerializer
+    {
+        void Serialize<T>(Stream dest, T value);
+        object Deserialize(Stream source, Type type);
+    }
+}
