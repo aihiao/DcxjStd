6863a121b76d5e64a9d39c92b3b4154878e1e17f
liyangwei999 <1611086176@qq.com>
2019-08-03 19:10:54 +0800

    GCLoginGameMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/GCLoginGameMessage.cs
@@ -0,0 +1,16 @@
+﻿using LywGames.Messages.Proto.Game;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 131075, IsRequest = false)]
+    public class GCLoginGameMessage : ProtocolMessage<LoginGameRes>
+    {
+        public override int Result
+        {
+            get
+            {
+                return protocol.result;
+            }
+        }
+    }
+}
