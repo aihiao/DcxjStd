3e1c27dc538b20197d66725c8ebc3be7e92f0f8f
liyangwei999 <1611086176@qq.com>
2019-08-03 10:37:30 +0800

    MetaDataProtobufSerializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/MetaDataProtobufSerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+
+namespace LywGames.Messages
+{
+    public class MetaDataProtobufSerializer : IProtobufSerializer
+    {
+        public void Serialize<T>(Stream dest, T value)
+        {
+            Serializer.Serialize<T>(dest, value);
+        }
+
+        public object Deserialize(Stream source, Type type)
+        {
+            return Serializer.NonGeneric.Deserialize(type, source);
+        }
+    }
+}
