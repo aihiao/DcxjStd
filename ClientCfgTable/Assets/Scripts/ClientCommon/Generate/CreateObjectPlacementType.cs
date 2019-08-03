05ce2a6c034b282a9116aabe51bccd218eb8ac05
liyangwei999 <1611086176@qq.com>
2019-08-03 09:39:22 +0800

    CreateObjectPlacementType

---
+++ ClientCfgTable/Assets/Scripts/ClientCommon/Generate/CreateObjectPlacementType.cs
@@ -0,0 +1,32 @@
+﻿namespace ClientCommon
+{
+    public sealed class CreateObjectPlacementType : TypeNameContainer<CreateObjectPlacementType>
+    {
+        /// <summary>
+        /// 0
+        /// </summary>
+        public const int Unknown = 0;
+        /// <summary>
+        /// 0
+        /// </summary>
+        public const int AtTarget = 1;
+        /// <summary>
+        /// 0
+        /// </summary>
+        public const int AtSource = 2;
+
+        public static void Initialize()
+        {
+            if (initialized)
+                return;
+
+            SetTextSectionName("TypeDef_CreateObjectPlacementType");
+
+            RegisterType("Unknown", Unknown, "无效");
+            RegisterType("AtTarget", AtTarget, "目标");
+            RegisterType("AtSource", AtSource, "源");
+
+            initialized = true;
+        }
+    }
+}
