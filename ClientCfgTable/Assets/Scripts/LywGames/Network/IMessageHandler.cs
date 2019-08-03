22d9810498cf76b102cf4801e5f17281054d64e8
liyangwei999 <1611086176@qq.com>
2019-08-03 20:00:08 +0800

    IMessageHandler

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/IMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Sockets;
+using LywGames.Messages;
+
+namespace LywGames.Network
+{
+    public interface IMessageHandler
+    {
+        void handleMessage(IConnection connection, Message message);
+        void handleConnectionActive(IConnection connection, SocketError result);
+        void handleConnectionInactive(IConnection connection, SocketError result);
+        void handleRequestTimeout(IConnection connection, int userData);
+    }
+}
