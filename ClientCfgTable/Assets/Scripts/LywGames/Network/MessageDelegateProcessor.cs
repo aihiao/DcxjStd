3903cfc4395a266586a5bd91b14a010ca1f2094b
liyangwei999 <1611086176@qq.com>
2019-08-03 19:46:38 +0800

    MessageDelegateProcessor

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/MessageDelegateProcessor.cs
@@ -0,0 +1,37 @@
+﻿using LywGames.Messages;
+
+namespace LywGames.Network
+{
+    public class MessageDelegateProcessor
+    {
+        private MessageDelegateInitializer delegateInitializer;
+
+        public MessageDelegateProcessor(MessageDelegateInitializer delegateInitializer)
+        {
+            this.delegateInitializer = delegateInitializer;
+        }
+
+        public bool HandleMessage(Message msg, IConnection connection)
+        {
+            MessageDelegateNode messageDelegate = delegateInitializer.getMessageDelegate(msg.GetType());
+
+            bool result;
+            if (messageDelegate == null)
+            {
+                result = false;
+            }
+            else
+            {
+                messageDelegate.ReceiveMessage(msg);
+                if (messageDelegate.isShortConnect)
+                {
+                    connection.Disconnect();
+                }
+                result = true;
+            }
+
+            return result;
+        }
+
+    }
+}
