2ab38704e9a6cac47ee0245e4b1c6c940a72109d
liyangwei999 <1611086176@qq.com>
2019-08-03 19:47:05 +0800

    ServerBusiness

---
+++ ClientCfgTable/Assets/Scripts/Business/ServerBusiness.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using LywGames.Messages;
+using LywGames.Messages.Proto.Auth;
+using LywGames.Messages.Proto.Game;
+
+public partial class ServerBusiness
+{
+    public void InitializeRegisters()
+    {
+
+    }
+
+    public void PrintBusinessLog(string log)
+    {
+        LoggerManager.Instance.Info(log);
+    }
+
+    public bool GetLoginGsResult(out GCLoginGameMessage loginRes, out bool isNeedQueryData)
+    {
+        return protocol.GetLogiGsResult(out loginRes, out isNeedQueryData);
+    }
+
+}
