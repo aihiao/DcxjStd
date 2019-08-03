882d28001d8d6baaa7c14066b9f2bcf56d5f3f2f
liyangwei999 <1611086176@qq.com>
2019-08-03 19:46:23 +0800

    MessageDelegateNode

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/MessageDelegateNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LywGames.Messages;
+
+namespace LywGames.Network
+{
+    public class MessageDelegateNode
+    {
+        public Action<Message> receiveAction;
+        public bool isShortConnect = false;
+        public void ReceiveMessage(Message message)
+        {
+            receiveAction(message);
+        }
+    }
+}
