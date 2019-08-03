944a0bd33603a9d6cbcefcce237373cb0e482106
liyangwei999 <1611086176@qq.com>
2019-08-03 19:46:56 +0800

    public const string UiPnlTipIndicator = "UiPnlTipIndicator";

--- ClientCfgTable/Assets/Scripts/Constants/UiPrefabNames.cs
+++ ClientCfgTable/Assets/Scripts/Constants/UiPrefabNames.cs
@@ -8,4 +8,5 @@
     public const string UiPnlModelBackground = "UiPnlModelBackground";
     public const string UiPnlReconnectMessage = "UiPnlReconnectMessage";
     public const string UiPnlLogin = "UiPnlLogin";
+    public const string UiPnlTipIndicator = "UiPnlTipIndicator";
 }
