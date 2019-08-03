2c9af499ba955ee8dc7f8cb41edccdefed0e4c05
liyangwei999 <1611086176@qq.com>
2019-08-03 19:08:37 +0800

    登录信息的获取和存储

--- ClientCfgTable/Assets/Scripts/Model/LocalData/LoginInfo.cs
+++ ClientCfgTable/Assets/Scripts/Model/LocalData/LoginInfo.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using LywGames.Message.Proto.Auth;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using LywGames.Messages;
+using LywGames.Messages.Proto.Auth;
 
 public class LoginInfo
 {
@@ -10,6 +10,20 @@ public class LoginInfo
     {
         get { return selectedArea; }
         set { selectedArea = value; }
+    }
+
+    private List<LoginRes.AreaPro> areas;
+    public List<LoginRes.AreaPro> Areas
+    {
+        get { return areas; }
+        set { areas = value; }
+    }
+
+    private LoginRes.ChannelMessage channel;
+    public LoginRes.ChannelMessage Channel
+    {
+        get { return channel; }
+        set { channel = value; }
     }
 
     private string account;
@@ -26,11 +40,11 @@ public class LoginInfo
         set { passWord = value; }
     }
 
-    private long accountID;
-    public long AccountID
+    private long accountId;
+    public long AccountId
     {
-        get { return accountID; }
-        set { accountID = value; }
+        get { return accountId; }
+        set { accountId = value; }
     }
 
     private bool isAutoLogin;
@@ -82,14 +96,32 @@ public class LoginInfo
         set { token = value; }
     }
 
+    public void SetLoginAuthMessage(ACLoginAuthMessage message)
+    {
+        accountId = message.Protocol.accountID;
+        areas = message.Protocol.areas;
+        channel = message.Protocol.channel;
+        isFirstQuickLogin = message.Protocol.isFirstQuickLogin;
+        isShowActivityInterface = message.Protocol.isShowActiveInterface;
+        lastAreaId = message.Protocol.lastAreaID;
+        result = message.Protocol.result;
+        token = message.Protocol.token;
+    }
+
     public void Initialize()
     {
-
+        Account = PlayerPrefs.GetString(Defines.gdAcc);
+        PassWord = PlayerPrefs.GetString(Defines.gdPwd);
+        isQuickLogin = PlayerPrefs.GetInt(Defines.gdQuickLogin) == 1 ? true : false;
+        IsAutoLogin = PlayerPrefs.GetInt(Defines.gdHasAccountLogined) == 1 ? true : false;
     }
 
     public void Save()
     {
-
+        PlayerPrefs.SetString(Defines.gdAcc, Account);
+        PlayerPrefs.SetString(Defines.gdPwd, PassWord);
+        PlayerPrefs.SetInt(Defines.gdQuickLogin, isQuickLogin ? 1 : 0);
+        PlayerPrefs.SetInt(Defines.gdHasAccountLogined, IsAutoLogin ? 1 : 0);
     }
 
 }
