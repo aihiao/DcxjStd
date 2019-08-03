7c6c4395a77eb7776e3ecf8343269e2130f0ea5e
liyangwei999 <1611086176@qq.com>
2019-08-03 10:37:24 +0800

    TypeModelProtobufSerializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/TypeModelProtobufSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using ProtoBuf.Meta;
+
+namespace LywGames.Messages
+{
+    public class TypeModelProtobufSerializer : IProtobufSerializer
+    {
+        private TypeModel typeModelSerializer = null;
+        public TypeModelProtobufSerializer(TypeModel typeMode)
+        {
+            this.typeModelSerializer = typeMode;
+        }
+
+        public void Serialize<T>(Stream dest, T value)
+        {
+            typeModelSerializer.Serialize(dest, value);
+        }
+
+        public object Deserialize(Stream source, Type type)
+        {
+            return typeModelSerializer.Deserialize(source, null, type);
+        }
+
+    }
+}
