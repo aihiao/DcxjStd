431d116f07dc80873dbde73f299ba04aaa759f7f
liyangwei999 <1611086176@qq.com>
2019-08-03 20:00:24 +0800

    ClientHelperMessageInitializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/ClientHelper/ClientHelperMessageInitializer.cs
@@ -0,0 +1,16 @@
+﻿using LywGames.Network;
+using LywGames.Messages;
+
+namespace LywGames.ClientHelper
+{
+    public class ClientHelperMessageInitializer : AbstractMessageInitializer
+    {
+        public override void Initilial()
+        {
+            base.AddMessage(typeof(ACLoginAuthMessage));
+       
+            base.AddMessage(typeof(GCLoginGameMessage));
+           
+        }
+    }
+}
