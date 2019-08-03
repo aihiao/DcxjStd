02fe6d3ea90c86ba515a9bbc9bf312b49e2379fd
liyangwei999 <1611086176@qq.com>
2019-08-03 19:46:46 +0800

    MessageDelegateInitializer

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/MessageDelegateInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LywGames.Network
+{
+    public class MessageDelegateInitializer
+    {
+        private Dictionary<Type, MessageDelegateNode> msgReceiveDelegates = new Dictionary<Type, MessageDelegateNode>();
+
+        public void AddMessageReceiveDelegate(Type msgType, MessageDelegateNode msgDelegate)
+        {
+            this.msgReceiveDelegates.Add(msgType, msgDelegate);
+        }
+
+        public MessageDelegateNode getMessageDelegate(Type msgType)
+        {
+            MessageDelegateNode result = null;
+            msgReceiveDelegates.TryGetValue(msgType, out result);
+            return result;
+        }
+
+    }
+}
