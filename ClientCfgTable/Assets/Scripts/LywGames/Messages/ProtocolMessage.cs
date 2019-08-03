2812d0d95c52160e5c96b41bf87a22191aba9b68
liyangwei999 <1611086176@qq.com>
2019-08-03 10:38:08 +0800

    ProtocolMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/ProtocolMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LywGames.Common;
+
+namespace LywGames.Messages
+{
+    public class ProtocolMessage <T> : Message where T : new()
+    {
+        protected T protocol = (default(T) == null) ? Activator.CreateInstance<T>() : default(T);
+        public T Protocol
+        {
+            get
+            {
+                return protocol;
+            }
+        }
+
+        public override Message DecodeBody(byte[] buffer, int offset, int count)
+        {
+            protocol = (T)((object)MySerializer.GetInstance().DeserializeByteBuffer(buffer, offset, count, typeof(T)));
+            return this;
+        }
+
+        public override void EncodeBody(NetworkBuffer outbuffer)
+        {
+            MySerializer.GetInstance().Serialize<T>(outbuffer, this.protocol);
+        }
+    }
+}
