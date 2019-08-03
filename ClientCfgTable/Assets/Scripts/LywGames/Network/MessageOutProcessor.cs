52b2734fa9a51ad7ae76623bdfc813e769ecadb5
liyangwei999 <1611086176@qq.com>
2019-08-03 20:00:02 +0800

    MessageOutProcessor

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/MessageOutProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace LywGames.Network
+{
+    public class MessageOutProcessor : AbstractNetworkOutHandler
+    {
+        public override void Send(IConnection connection, byte[] buffer, int offset, int size)
+        {
+            base.sendBuffDown(connection, buffer, offset, size);
+        }
+        public override void Send(IConnection connection, object msg)
+        {
+            base.sendObjectDown(connection, msg);
+        }
+    }
+}
