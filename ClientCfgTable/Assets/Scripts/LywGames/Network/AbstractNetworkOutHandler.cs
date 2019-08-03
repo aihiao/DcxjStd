fb117b885fbb896c1b32a7c28aad0464120a7eb3
liyangwei999 <1611086176@qq.com>
2019-08-03 19:46:08 +0800

    AbstractNetworkOutHandler

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/AbstractNetworkOutHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LywGames.Network
+{
+    public abstract class AbstractNetworkOutHandler
+    {
+        protected object obj;
+        private AbstractNetworkOutHandler nextOutHandler;
+        public AbstractNetworkOutHandler NextOutHandler
+        {
+            get
+            {
+                return this.nextOutHandler;
+            }
+            set
+            {
+                this.nextOutHandler = value;
+            }
+        }
+
+        protected void sendBuffDown(IConnection connection, byte[] buffer, int offset, int size)
+        {
+            if (this.NextOutHandler != null)
+            {
+                this.NextOutHandler.Send(connection, buffer, offset, size);
+            }
+            else
+            {
+                connection._Send(buffer, offset, size);
+            }
+        }
+
+        protected void sendObjectDown(IConnection connection, object msg)
+        {
+            if (this.NextOutHandler != null)
+            {
+                this.NextOutHandler.Send(connection, msg);
+                return;
+            }
+            throw new NotSupportedException("The last ConnectionOutHandler can't be invoked Send(IConnection connection, Object msg)");
+        }
+
+        public abstract void Send(IConnection connection, byte[] buffer, int offset, int size);
+        public abstract void Send(IConnection connection, object msg);
+    }
+}
