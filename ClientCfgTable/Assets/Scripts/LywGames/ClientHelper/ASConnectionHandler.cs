0974b268df1e3b7cab5613e332476632f9682efa
liyangwei999 <1611086176@qq.com>
2019-08-04 10:37:10 +0800

    ASConnectionHandler

---
+++ ClientCfgTable/Assets/Scripts/LywGames/ClientHelper/ASConnectionHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Sockets;
+using LywGames.Messages;
+using LywGames.Network;
+
+namespace LywGames.ClientHelper
+{
+    public class ASConnectionHandler : AbstractConnectionHandler
+    {
+        private Message req;
+        private ClientHelper clientHelper;
+
+        public ASConnectionHandler(Message req, ClientHelper clientHelper)
+        {
+            this.req = req;
+            this.clientHelper = clientHelper;
+        }
+
+        public override void handleConnectionActive(IConnection connection, SocketError result)
+        {
+            if (result == SocketError.Success)
+            {
+                LoggerManager.Instance.Debug("AsConnection {0} Active then send message", new object[]
+                {
+                    connection.Remote.Address
+                });
+                connection.Send(this.req, 1);
+            }
+            else
+            {
+                this.clientHelper.OnAsConnectFailed();
+            }
+        }
+
+        public override void handleConnectionInactive(IConnection connection, SocketError result)
+        {
+            this.clientHelper.OnAsConnectFailed();
+        }
+
+        public override void handleRequestTimeout(IConnection connection, int userData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
