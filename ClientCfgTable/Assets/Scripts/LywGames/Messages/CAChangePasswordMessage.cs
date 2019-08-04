bc50bbeefde5348134e9e373dc4be5f96cb82720
liyangwei999 <1611086176@qq.com>
2019-08-04 10:36:54 +0800

    CAChangePasswordMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CAChangePasswordMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65544, IsRequest = true)]
+    public class CAChangePasswordMessage : ProtocolMessage<ChangePasswordReq>
+    {
+    }
+}
