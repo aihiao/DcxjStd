edbd561be058229c638882fc942d61a0a095cac0
liyangwei999 <1611086176@qq.com>
2019-08-04 10:36:45 +0800

    CABindAccountMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CABindAccountMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65542, IsRequest = true)]
+    public class CABindAccountMessage : ProtocolMessage<BindAccountReq>
+    {
+    }
+}
