f8e8d75b4563b39f74f3ac686036d8aae802634b
liyangwei999 <1611086176@qq.com>
2019-08-04 10:35:22 +0800

    CBMessageReq

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Messages/Game/CBMessageReq.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using ProtoBuf;
+
+namespace LywGames.Messages.Proto.Game
+{
+    [ProtoContract(Name = "CBMessageReq")]
+    public class CBMessageReq : IExtensible
+    {
+        private byte[] _Buffer = null;
+        private IExtension extensionObject;
+        [ProtoMember(1), DefaultValue(null)]
+        public byte[] Buffer
+        {
+            get
+            {
+                return this._Buffer;
+            }
+            set
+            {
+                this._Buffer = value;
+            }
+        }
+        public byte[] getBuffer()
+        {
+            return this._Buffer;
+        }
+        public void setBuffer(byte[] value)
+        {
+            this._Buffer = value;
+        }
+        IExtension IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return Extensible.GetExtensionObject(ref this.extensionObject, createIfMissing);
+        }
+    }
+}
