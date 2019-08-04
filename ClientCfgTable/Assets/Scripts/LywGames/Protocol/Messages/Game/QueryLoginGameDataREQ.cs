d98e14c66633c48b07258bc07ab89e30f583cd97
liyangwei999 <1611086176@qq.com>
2019-08-04 10:36:26 +0800

    QueryLoginGameDataREQ

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Messages/Game/QueryLoginGameDataREQ.cs
@@ -0,0 +1,14 @@
+﻿using ProtoBuf;
+
+namespace LywGames.Messages.Proto.Game
+{
+    [ProtoContract(Name = "QueryLoginGameDataREQ")]
+    public class QueryLoginGameDataREQ : IExtensible
+    {
+        private IExtension extensionObject;
+        IExtension IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return Extensible.GetExtensionObject(ref this.extensionObject, createIfMissing);
+        }
+    }
+}
