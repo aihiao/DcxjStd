b2a1a1e221b4e76b6402f9df86a54fc0bca7d6cd
liyangwei999 <1611086176@qq.com>
2019-08-04 14:40:05 +0800

    NetworkManager

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/NetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+
+namespace LywGames.Network
+{
+    public class NetworkManager : INetworkManager
+    {
+        private static NetworkManager instance = new NetworkManager();
+        public static INetworkManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        public static INetworkManager GetInstance()
+        {
+            return instance;
+        }
+
+        public IConnection CreateConnection(ProtocolType type, int timeout)
+        {
+            IConnection result;
+            if (type == ProtocolType.Tcp)
+            {
+                result = new TCPConnection(timeout);
+            }
+            else
+            {
+                if (type == ProtocolType.Udp)
+                {
+                    result = new UdpAdpaterConnection(timeout);
+                }
+                else
+                {
+                    result = null;
+                }
+            }
+            return result;
+        }
+
+        public IConnection SearchConnectoin(int connectionID)
+        {
+            return null;
+        }
+
+        public bool RemoveConnection(int connectionID)
+        {
+            return true;
+        }
+    }
+}
