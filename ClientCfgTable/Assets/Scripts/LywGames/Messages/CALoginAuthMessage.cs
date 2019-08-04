e8b76345dae57ea1c970ad880a133e20234abb19
liyangwei999 <1611086176@qq.com>
2019-08-04 10:37:55 +0800

    CALoginAuthMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CALoginAuthMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65538, IsRequest = true)]
+    public class CALoginAuthMessage : ProtocolMessage<LoginReq>
+    {
+    }
+}
