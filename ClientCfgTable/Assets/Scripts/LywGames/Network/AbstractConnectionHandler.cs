2eb9197616ee4a29314affb994b013424d4fcf6b
liyangwei999 <1611086176@qq.com>
2019-08-04 10:37:18 +0800

    AbstractConnectionHandler

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/AbstractConnectionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+using LywGames.Messages;
+
+namespace LywGames.Network
+{
+    public abstract class AbstractConnectionHandler : IMessageHandler
+    {
+        public virtual void handleMessage(IConnection connection, Message message)
+        {
+        }
+
+        public virtual void handleConnectionActive(IConnection connection, SocketError result)
+        {
+        }
+
+        public abstract void handleConnectionInactive(IConnection connection, SocketError result);
+        public abstract void handleRequestTimeout(IConnection connection, int userData);
+    }
+}
