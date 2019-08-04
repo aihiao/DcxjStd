274cf0175a6fcbd85eede55ecc96ecdd57d02fb4
liyangwei999 <1611086176@qq.com>
2019-08-04 19:02:03 +0800

    GetConfigVersions()

--- ClientCfgTable/Assets/Scripts/Globle/GameUtility.cs
+++ ClientCfgTable/Assets/Scripts/Globle/GameUtility.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
 using ClientCommon;
+using LywGames.Messages.Proto.Game;
 
 public static class GameUtility
 {
@@ -12,4 +13,30 @@ public static class GameUtility
         }
         return "Can't find string for key:" + key;
     }
+
+    public static List<TableVersion> GetConfigVersions()
+    {
+        List<TableVersion> versions = new List<TableVersion>();
+
+        foreach (var version in ConfigDataBase.VersionConfig.Versions)
+        {
+            TableVersion ver = new TableVersion();
+            ver.tableName = version.TableName;
+            ver.versionId = version.VersionText;
+
+            versions.Add(ver);
+        }
+
+        foreach (var gmVersion in ConfigDataBase.GmVersionConfig.GmVersions)
+        {
+            TableVersion ver = new TableVersion();
+            ver.tableName = gmVersion.TableName;
+            ver.versionId = gmVersion.VersionText;
+
+            versions.Add(ver);
+        }
+
+        return versions;
+    }
+
 }
