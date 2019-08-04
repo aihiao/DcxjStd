25afdbe59f40c00d5d3a559f1f999d167a56298c
liyangwei999 <1611086176@qq.com>
2019-08-04 10:37:24 +0800

    CBMessageReqMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CBMessageReqMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Game;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 196609, IsRequest = false)]
+    public class CBMessageReqMessage : ProtocolMessage<CBMessageReq>
+    {
+    }
+}
