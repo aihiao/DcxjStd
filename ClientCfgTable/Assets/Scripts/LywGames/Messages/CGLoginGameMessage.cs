3843c76ccd5e5d6ad23c9c76cb49e7ec51cd0e32
liyangwei999 <1611086176@qq.com>
2019-08-04 10:37:47 +0800

    CGLoginGameMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CGLoginGameMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Game;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 131074, IsRequest = true)]
+    public class CGLoginGameMessage : ProtocolMessage<LoginGameReq>
+    {
+    }
+}
