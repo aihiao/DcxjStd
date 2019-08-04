2f6c8c3c2049fc6d68ea0a204a6876f537068498
liyangwei999 <1611086176@qq.com>
2019-08-04 10:37:01 +0800

    CACreateAccountMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CACreateAccountMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65540, IsRequest = true)]
+    public class CACreateAccountMessage : ProtocolMessage<CreateAccountReq>
+    {
+    }
+}
