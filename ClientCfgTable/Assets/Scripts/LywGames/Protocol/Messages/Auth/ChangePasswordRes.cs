01cc7bb039929004ea6e7d8cc5706860c4286707
liyangwei999 <1611086176@qq.com>
2019-08-04 10:35:39 +0800

    ChangePasswordRes

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Messages/Auth/ChangePasswordRes.cs
@@ -0,0 +1,27 @@
+﻿using ProtoBuf;
+
+namespace LywGames.Messages.Proto.Auth
+{
+    [ProtoContract(Name = "ChangePasswordRes")]
+    public class ChangePasswordRes : IExtensible
+    {
+        private int _result;
+        private IExtension extensionObject;
+        [ProtoMember(1)]
+        public int result
+        {
+            get
+            {
+                return this._result;
+            }
+            set
+            {
+                this._result = value;
+            }
+        }
+        IExtension IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return Extensible.GetExtensionObject(ref this.extensionObject, createIfMissing);
+        }
+    }
+}
