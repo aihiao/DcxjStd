6fff56ed35a130629208c58143a0795bb9876d5a
liyangwei999 <1611086176@qq.com>
2019-08-04 10:36:38 +0800

    CAActiveCodeMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/CAActiveCodeMessage.cs
@@ -0,0 +1,9 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65546, IsRequest = true)]
+    public class CAActiveCodeMessage : ProtocolMessage<ActiveCodeReq>
+    {
+    }
+}
