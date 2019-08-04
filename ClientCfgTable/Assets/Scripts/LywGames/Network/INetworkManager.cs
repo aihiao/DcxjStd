ddc8de010e4e9b00b2f3a9764187bf5b728b1f3a
liyangwei999 <1611086176@qq.com>
2019-08-04 14:40:12 +0800

    INetworkManager

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/INetworkManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace LywGames.Network
+{
+    public interface INetworkManager
+    {
+        IConnection CreateConnection(ProtocolType type, int userData);
+        IConnection SearchConnectoin(int connectionID);
+        bool RemoveConnection(int connectionID);
+    }
+}
