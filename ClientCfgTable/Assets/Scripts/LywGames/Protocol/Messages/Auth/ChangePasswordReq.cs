274b778dbe433f70932e94c0c34683ea521b1714
liyangwei999 <1611086176@qq.com>
2019-08-04 10:35:31 +0800

    ChangePasswordReq

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Messages/Auth/ChangePasswordReq.cs
@@ -0,0 +1,53 @@
+﻿using ProtoBuf;
+
+namespace LywGames.Messages.Proto.Auth
+{
+    [ProtoContract(Name = "ChangePasswordReq")]
+    public class ChangePasswordReq : IExtensible
+    {
+        private string _email;
+        private string _newPassword;
+        private string _oldPassword;
+        private IExtension extensionObject;
+        [ProtoMember(1)]
+        public string email
+        {
+            get
+            {
+                return this._email;
+            }
+            set
+            {
+                this._email = value;
+            }
+        }
+        [ProtoMember(1)]
+        public string newPassword
+        {
+            get
+            {
+                return this._newPassword;
+            }
+            set
+            {
+                this._newPassword = value;
+            }
+        }
+        [ProtoMember(1)]
+        public string oldPassword
+        {
+            get
+            {
+                return this._oldPassword;
+            }
+            set
+            {
+                this._oldPassword = value;
+            }
+        }
+        IExtension IExtensible.GetExtensionObject(bool createIfMissing)
+        {
+            return Extensible.GetExtensionObject(ref this.extensionObject, createIfMissing);
+        }
+    }
+}
