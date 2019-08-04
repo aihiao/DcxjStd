966c71ecfe0283142d6a2e55ab792dd329de4adc
liyangwei999 <1611086176@qq.com>
2019-08-04 19:03:46 +0800

    LoginGS

--- ClientCfgTable/Assets/Scripts/Business/ServerBusiness.cs
+++ ClientCfgTable/Assets/Scripts/Business/ServerBusiness.cs
@@ -16,6 +16,21 @@ public partial class ServerBusiness
         LoggerManager.Instance.Info(log);
     }
 
+    /**
+	 * 登陆游戏服务器
+	 * b)	调用LoginGS，有三个关键输入参数都要输入
+		i.	Token
+		ii.	KOD文件的版本信息列表（包括运营配置）
+		iii.	是登录还是断线重连
+	 */
+    public bool LoginGS(string hostname, int port, int callback, long accountID, int areadId, string token)
+    {
+        PrintBusinessLog("[ServerBusiness] LoginGS" + " IP: " + hostname + "  port: " + port.ToString());
+        List<TableVersion> versions = GameUtility.GetConfigVersions();
+        bool result = protocol.LoginGS(hostname, port, callback, accountID, areadId, token, versions);
+        return result;
+    }
+
     public bool GetLoginGsResult(out GCLoginGameMessage loginRes, out bool isNeedQueryData)
     {
         return protocol.GetLogiGsResult(out loginRes, out isNeedQueryData);
