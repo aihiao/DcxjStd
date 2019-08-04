6a38a832678e56ee5ee49b3cd6dced4c868b06d1
liyangwei999 <1611086176@qq.com>
2019-08-04 14:39:58 +0800

    新增Protocols_c.cs

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Protocol/Protocols_c.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ProtoBuf;
+using ProtoBuf.Meta;
+
+namespace LywGames.Protocol
+{
+    public class Protocols_c : TypeModel
+    {
+        protected override object Deserialize(int key, object value, ProtoReader source)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int GetKeyImpl(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Serialize(int key, object value, ProtoWriter dest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
