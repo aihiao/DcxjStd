5e46a59bde052a9af5723f07c19367575d033a09
liyangwei999 <1611086176@qq.com>
2019-08-04 15:53:30 +0800

    CloseIndicatorIfShowing ShowIndicatorIfNot

--- ClientCfgTable/Assets/Scripts/View/Views/Ui/UiPnlTipIndicator.cs
+++ ClientCfgTable/Assets/Scripts/View/Views/Ui/UiPnlTipIndicator.cs
@@ -3,5 +3,20 @@ using System.Collections;
 
 public class UiPnlTipIndicator : BaseUi
 {
+    public static void CloseIndicatorIfShowing()
+    {
+        if (UiManager.Instance.GetIsShowing<UiPnlTipIndicator>())
+        {
+            UiManager.Instance.Hide<UiPnlTipIndicator>();
+        }
+    }
+
+    public static void ShowIndicatorIfNot()
+    {
+        if (!UiManager.Instance.GetIsShowing<UiPnlTipIndicator>())
+        {
+            UiManager.Instance.ShowByName(UiPrefabNames.UiPnlTipIndicator);
+        }
+    }
 
 }
