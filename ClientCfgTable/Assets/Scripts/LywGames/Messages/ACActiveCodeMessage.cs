6c04d249f0f83c58f5020a8d332d0eb9737b3572
liyangwei999 <1611086176@qq.com>
2019-08-05 16:19:56 +0800

    ACActiveCodeMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/ACActiveCodeMessage.cs
@@ -0,0 +1,16 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65547, IsRequest = false)]
+    public class ACActiveCodeMessage : ProtocolMessage<ActiveCodeRes>
+    {
+        public override int Result
+        {
+            get
+            {
+                return protocol.result;
+            }
+        }
+    }
+}
