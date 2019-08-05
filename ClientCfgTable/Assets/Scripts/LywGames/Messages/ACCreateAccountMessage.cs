c01f9ba6b76b6e0cec81df1b5cc5597e119963ab
liyangwei999 <1611086176@qq.com>
2019-08-05 16:20:04 +0800

    ACCreateAccountMessage

---
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/ACCreateAccountMessage.cs
@@ -0,0 +1,16 @@
+﻿using LywGames.Messages.Proto.Auth;
+
+namespace LywGames.Messages
+{
+    [Message(ProtocolId = 65541, IsRequest = false)]
+    public class ACCreateAccountMessage : ProtocolMessage<CreateAccountRes>
+    {
+        public override int Result
+        {
+            get
+            {
+                return protocol.result;
+            }
+        }
+    }
+}
