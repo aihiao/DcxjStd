4ec1ffbba7ba924d1ab70c482faaa5fbf1da740a
liyangwei999 <1611086176@qq.com>
2019-08-05 18:10:22 +0800

    选区状态机

---
+++ ClientCfgTable/Assets/Scripts/GameStateMachine/GameStateSelectArea.cs
@@ -0,0 +1,25 @@
+﻿public class GameStateSelectArea : GameStateBase
+{
+    public override bool IsGamingState
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override GameStateType StateType
+    {
+        get
+        {
+            return GameStateType.SelectArea;
+        }
+    }
+
+    public GameStateSelectArea StartUi()
+    {
+        return this;
+    }
+
+}
+
