0288a25abaed9332ca762bdf6a09534d83a47a2b
liyangwei999 <1611086176@qq.com>
2019-08-05 18:10:08 +0800

    登录状态机

---
+++ ClientCfgTable/Assets/Scripts/GameStateMachine/GameStateLogin.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class GameStateLogin : GameStateBase
+{
+    public override bool IsGamingState
+    {
+        get
+        {
+            return false;
+        }
+    }
+
+    public override GameStateType StateType
+    {
+        get
+        {
+            return GameStateType.Login;
+        }
+    }
+
+    public void OnLoginSuccess(PRLoginAS prlogin)
+    {
+
+    }
+
+    public void OnLoginFailed(int errCode, string errMsg)
+    {
+
+    }
+
+    public void CreateAccount(string account, string password, bool isPasswordEncrypted, int passwordLength)
+    {
+
+    }
+
+    public void OnCreateAccountSuccess(string account, string password, bool isPasswordEncrypted, int passwordLength)
+    {
+
+    }
+
+    public void OnCreateAccountFalied(int errCode, string errMsg)
+    {
+
+    }
+
+}
