e99959f229c4dd07468a55a37067502e688acd62
liyangwei999 <1611086176@qq.com>
2019-08-06 16:32:10 +0800

    ResultCode

--- ClientCfgTable/Assets/Scripts/LywGames/Messages/ACLoginAuthMessage.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/ACLoginAuthMessage.cs
@@ -5,11 +5,11 @@ namespace LywGames.Messages
     [Message(ProtocolId = 65539, IsRequest = false)]
     public class ACLoginAuthMessage : ProtocolMessage<LoginRes>
     {
-        public override int Result
+        public override int ResultCode
         {
             get
             {
-                return protocol.result;
+                return protocol.ResultCode;
             }
         }
     }
