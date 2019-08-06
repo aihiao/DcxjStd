ed9cfff9a2f73477b3006fc3ab2b046e69befa80
liyangwei999 <1611086176@qq.com>
2019-08-06 16:32:02 +0800

    ResultCode

--- ClientCfgTable/Assets/Scripts/LywGames/Messages/ACCreateAccountMessage.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/ACCreateAccountMessage.cs
@@ -5,11 +5,11 @@ namespace LywGames.Messages
     [Message(ProtocolId = 65541, IsRequest = false)]
     public class ACCreateAccountMessage : ProtocolMessage<CreateAccountRes>
     {
-        public override int Result
+        public override int ResultCode
         {
             get
             {
-                return protocol.result;
+                return protocol.ResultCode;
             }
         }
     }
