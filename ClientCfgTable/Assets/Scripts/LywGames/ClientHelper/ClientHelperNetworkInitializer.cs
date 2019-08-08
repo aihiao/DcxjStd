e551b4948ea9a0145f7c8cc7dbf2d84aad2a57b8
liyangwei999 <1611086176@qq.com>
2019-08-08 11:53:40 +0800

    protocolType

--- ClientCfgTable/Assets/Scripts/LywGames/ClientHelper/ClientHelperNetworkInitializer.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/ClientHelper/ClientHelperNetworkInitializer.cs
@@ -7,24 +7,27 @@ namespace LywGames.ClientHelper
     {
         private AbstractMessageInitializer msgInitializer;
         private MessageDelegateProcessor msgDelegateProcessor;
-        private ProtocolType type;
-        public ClientHelperNetworkInitializer(AbstractMessageInitializer msgInitializer, MessageDelegateProcessor msgDelegateProcessor, ProtocolType type)
+        private ProtocolType protocolType;
+
+        public ClientHelperNetworkInitializer(AbstractMessageInitializer msgInitializer, MessageDelegateProcessor msgDelegateProcessor, ProtocolType protocolType)
         {
             this.msgInitializer = msgInitializer;
             this.msgDelegateProcessor = msgDelegateProcessor;
-            this.type = type;
+            this.protocolType = protocolType;
         }
+
         public override void Initial(NetworkHandlerPipeline pipeline, ConnectionType cnType)
         {
-            if (this.type != ProtocolType.Udp || (cnType != ConnectionType.CONNECTION_GAME && cnType != ConnectionType.CONNECTION_BATTLE))
+            if (protocolType != ProtocolType.Udp || (cnType != ConnectionType.CONNECTION_GAME && cnType != ConnectionType.CONNECTION_BATTLE))
             {
                 pipeline.AddHandler(new LengthFieldBasedFrameDecoder(204800, 0, 4, 0, 4));
                 pipeline.AddHandler(new LengthFieldPrepender(4, 0, false));
             }
             pipeline.AddHandler(new SnappyDecoder());
             pipeline.AddHandler(new SnappyEncoder());
-            pipeline.AddHandler(new MessageInProcessor(this.msgInitializer, this.msgDelegateProcessor));
+            pipeline.AddHandler(new MessageInProcessor(msgInitializer, msgDelegateProcessor));
             pipeline.AddHandler(new MessageOutProcessor());
         }
+
     }
 }
