ac4fcc7f01a13bae5263628495f71a16bcc65f25
liyangwei999 <1611086176@qq.com>
2019-08-08 11:49:38 +0800

    如果是短连接, 接收完消息, 断开连接。

--- ClientCfgTable/Assets/Scripts/LywGames/Network/MessageDelegateNode.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/MessageDelegateNode.cs
@@ -5,11 +5,13 @@ namespace LywGames.Network
 {
     public class MessageDelegateNode
     {
+        public bool isShortConnect = false; // 如果是短连接, 接收完消息, 断开连接。
+
         public Action<Message> receiveAction;
-        public bool isShortConnect = false;
         public void ReceiveMessage(Message message)
         {
             receiveAction(message);
         }
+
     }
 }
