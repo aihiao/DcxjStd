311e439432e84aa09b030d264694861bd9c55918
liyangwei999 <1611086176@qq.com>
2019-08-12 23:54:52 +0800

    public static int MaxRecvBufferSize = 102400; // 1024 * 100 = 100KB         public static int MaxSendBufferSize = 2048;         public static int MaxCompressMessageSize = 196611;         public static int MaxUncompressMessageSize = 1048576; // 1024 * 1024 = 1048576

--- ClientCfgTable/Assets/Scripts/LywGames/Messages/NetworkParameters.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/Messages/NetworkParameters.cs
@@ -2,9 +2,9 @@
 {
     public class NetworkParameters
     {
-        public static int _MAX_RECV_BUFFER_SIZE = 102400;
-        public static int _MAX_SEND_BUFFER_SIZE = 2048;
-        public static int _MAX_COMPRESS_MESSAGE_SIZE = 196611;
-        public static int _MAX_UNCOMPRESS_MESSAGE_SIZE = 1048576;
+        public static int MaxRecvBufferSize = 102400; // 1024 * 100 = 100KB
+        public static int MaxSendBufferSize = 2048;
+        public static int MaxCompressMessageSize = 196611;
+        public static int MaxUncompressMessageSize = 1048576; // 1024 * 1024 = 1048576
     }
 }
