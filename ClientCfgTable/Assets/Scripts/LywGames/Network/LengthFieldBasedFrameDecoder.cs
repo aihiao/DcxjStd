b2369bf94ee2e8e94904d8b3a97a558a683358bb
liyangwei999 <1611086176@qq.com>
2019-08-12 23:56:33 +0800

    优化

--- ClientCfgTable/Assets/Scripts/LywGames/Network/LengthFieldBasedFrameDecoder.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/Network/LengthFieldBasedFrameDecoder.cs
@@ -5,14 +5,15 @@ namespace LywGames.Network
 {
     public class LengthFieldBasedFrameDecoder : FrameDecoder
     {
-        private int maxFrameLength;
-        private int lengthFieldOffset;
-        private int lengthFieldLength;
+        private int maxFrameLength; // 最大的长度, 防止太大导致内存溢出
+        private int lengthFieldOffset; // 表示消息体长度的字段的偏移, 也就是Buffer的什么位置开始就是长度字段了
+        private int lengthFieldLength; // 长度字段的长度
         private int lengthFieldEndOffset;
-        private int lengthAdjustment;
-        private int initialBytesToStrip;
+        private int lengthAdjustment; // 有些情况可能会把消息头也包含到长度字段中, 或者长度字段后面还有一些不包括在长度字段内的, 可以通过lengthAdjustment调节
+        private int initialBytesToStrip; // 起始截掉的部分, 如果传递给后面的Handler的数据不需要消息头了, 可以通过这个设置
         private long tooLongFrameLength;
         private long bytesToDiscard;
+
         public LengthFieldBasedFrameDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength, int lengthAdjustment, int initialBytesToStrip)
         {
             if (maxFrameLength <= 0)
@@ -44,31 +45,34 @@ namespace LywGames.Network
                     ")."
                 }));
             }
+
             this.maxFrameLength = maxFrameLength;
             this.lengthFieldOffset = lengthFieldOffset;
             this.lengthFieldLength = lengthFieldLength;
             this.lengthAdjustment = lengthAdjustment;
-            this.lengthFieldEndOffset = lengthFieldOffset + lengthFieldLength;
             this.initialBytesToStrip = initialBytesToStrip;
+
+            lengthFieldEndOffset = lengthFieldOffset + lengthFieldLength;
         }
+
         public override bool Decode(NetworkBuffer msgBuffer, ref int msgCount)
         {
             bool result;
-            if (msgBuffer.ReadableBytes < this.lengthFieldEndOffset)
+            if (msgBuffer.ReadableBytes < lengthFieldEndOffset)
             {
                 result = false;
             }
             else
             {
-                int offset = msgBuffer.ReadOffset + this.lengthFieldOffset;
+                int offset = msgBuffer.ReadOffset + lengthFieldOffset;
                 int num;
-                switch (this.lengthFieldLength)
+                switch (lengthFieldLength)
                 {
                     case 1:
-                        num = (int)msgBuffer.GetByte(offset);
+                        num = msgBuffer.GetByte(offset);
                         goto IL_7A;
                     case 2:
-                        num = (int)msgBuffer.GetUInt16(offset);
+                        num = msgBuffer.GetUInt16(offset);
                         goto IL_7A;
                     case 4:
                         num = msgBuffer.GetInt32(offset);
@@ -78,41 +82,37 @@ namespace LywGames.Network
                 IL_7A:
                 if (num < 0)
                 {
-                    msgBuffer.SkipBytes(this.lengthFieldEndOffset);
+                    msgBuffer.SkipBytes(lengthFieldEndOffset);
                     throw new Exception("negative pre-adjustment length field: " + num);
                 }
-                num += this.lengthAdjustment + this.lengthFieldEndOffset;
-                if (num < this.lengthFieldEndOffset)
+                num += lengthAdjustment + lengthFieldEndOffset;
+                if (num < lengthFieldEndOffset)
                 {
-                    msgBuffer.SkipBytes(this.lengthFieldEndOffset);
+                    msgBuffer.SkipBytes(lengthFieldEndOffset);
                     throw new Exception(string.Concat(new object[]
                     {
                         "Adjusted frame length (",
                         num,
                         ") is less than lengthFieldEndOffset: ",
-                        this.lengthFieldEndOffset
+                        lengthFieldEndOffset
                     }));
                 }
-                if (num > this.maxFrameLength)
+                if (num > maxFrameLength)
                 {
-                    this.tooLongFrameLength = (long)num;
-                    this.bytesToDiscard = (long)(num - msgBuffer.ReadableBytes);
+                    tooLongFrameLength = (long)num;
+                    bytesToDiscard = (long)(num - msgBuffer.ReadableBytes);
                     msgBuffer.SkipBytes(msgBuffer.ReadableBytes);
-                    throw new Exception(string.Format("too long frame, frame length:{0}", this.tooLongFrameLength));
+                    throw new Exception(string.Format("too long frame, frame length:{0}", tooLongFrameLength));
                 }
                 int num2 = num;
                 if (msgBuffer.ReadableBytes < num2)
                 {
-                    LoggerManager.Instance.Info("msgBuffer.ReadableBytes {0} < frameLengthInt {1}", new object[]
-                    {
-                        msgBuffer.ReadableBytes,
-                        num2
-                    });
+                    LoggerManager.Instance.Info("msgBuffer.ReadableBytes {0} < frameLengthInt {1}", msgBuffer.ReadableBytes, num2);
                     result = false;
                 }
                 else
                 {
-                    if (this.initialBytesToStrip > num2)
+                    if (initialBytesToStrip > num2)
                     {
                         msgBuffer.SkipBytes(num2);
                         throw new Exception(string.Concat(new object[]
@@ -120,15 +120,17 @@ namespace LywGames.Network
                             "Adjusted frame length (",
                             num,
                             ") is less than initialBytesToStrip: ",
-                            this.initialBytesToStrip
+                            initialBytesToStrip
                         }));
                     }
-                    msgBuffer.SkipBytes(this.initialBytesToStrip);
-                    msgCount = num2 - this.initialBytesToStrip;
+                    msgBuffer.SkipBytes(initialBytesToStrip);
+                    msgCount = num2 - initialBytesToStrip;
                     result = true;
                 }
             }
+
             return result;
         }
+
     }
 }
