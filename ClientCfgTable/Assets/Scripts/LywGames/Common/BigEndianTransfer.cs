21960efe4a41a9bf7aabf7e189d13b27056750ad
liyangwei999 <1611086176@qq.com>
2019-08-12 23:48:19 +0800

    优化BigEndianTransfer

--- ClientCfgTable/Assets/Scripts/LywGames/Common/BigEndianTransfer.cs
+++ ClientCfgTable/Assets/Scripts/LywGames/Common/BigEndianTransfer.cs
@@ -4,36 +4,37 @@ namespace LywGames.Common
 {
     public class BigEndianTransfer
     {
-        private static byte[] ToFixedLength(byte[] a, int length)
+        private static byte[] ToFixedLength(byte[] data, int length)
         {
             byte[] result;
-            if (a.Length > length)
+            if (data.Length > length)
             {
                 result = null;
             }
             else
             {
-                if (a.Length == length)
+                if (data.Length == length)
                 {
-                    result = a;
+                    result = data;
                 }
                 else
                 {
-                    byte[] array = new byte[length];
-                    a.CopyTo(array, length - a.Length);
+                    byte[] array = new byte[length]; // 不够的bit用0补齐
+                    data.CopyTo(array, length - data.Length);
                     result = array;
                 }
             }
+
             return result;
         }
 
-        public static ulong ToUInt64(byte[] data)
+        public static short ToInt16(byte[] data)
         {
-            byte[] array = ToFixedLength(data, 8);
-            ulong result;
+            byte[] array = ToFixedLength(data, 2);
+            short result;
             if (array == null)
             {
-                result = 0uL;
+                result = 0;
             }
             else
             {
@@ -41,28 +42,18 @@ namespace LywGames.Common
                 {
                     Array.Reverse(array);
                 }
-                result = BitConverter.ToUInt64(array, 0);
+                result = BitConverter.ToInt16(array, 0);
             }
             return result;
         }
 
-        public static byte[] ToBytes(ulong num)
+        public static ushort ToUInt16(byte[] data)
         {
-            byte[] bytes = BitConverter.GetBytes(num);
-            if (BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(bytes);
-            }
-            return bytes;
-        }
-
-        public static long ToInt64(byte[] data)
-        {
-            byte[] array = ToFixedLength(data, 8);
-            long result;
+            byte[] array = ToFixedLength(data, 2);
+            ushort result;
             if (array == null)
             {
-                result = 0L;
+                result = 0;
             }
             else
             {
@@ -70,48 +61,42 @@ namespace LywGames.Common
                 {
                     Array.Reverse(array);
                 }
-                result = BitConverter.ToInt64(array, 0);
+                result = BitConverter.ToUInt16(array, 0);
             }
             return result;
         }
 
-        public static byte[] ToBytes(long num)
+        public static ushort ToUInt16(byte[] data, int offset)
         {
-            byte[] bytes = BitConverter.GetBytes(num);
-            if (BitConverter.IsLittleEndian)
+            ushort result;
+            if (data.Length <= offset)
             {
-                Array.Reverse(bytes);
-            }
-            return bytes;
-        }
-
-        public static uint ToUInt32(byte[] data)
-        {
-            byte[] array = ToFixedLength(data, 4);
-            uint result;
-            if (array == null)
-            {
-                result = 0u;
+                result = 0;
             }
             else
             {
                 if (BitConverter.IsLittleEndian)
                 {
-                    Array.Reverse(array);
+                    #region UInt16 一共2为, 下面这个高位和低位互换, 如果字节数不够, 高位用0填补。
+                    byte[] array = new byte[]
+                    {
+                        0,
+                        data[offset]
+                    };
+                    if (data.Length - offset > 1)
+                    {
+                        array[0] = data[offset + 1];
+                    }
+                    #endregion UInt16 一共2为, 下面这个高位和低位互换, 如果字节数不够, 高位用0填补。
+
+                    result = BitConverter.ToUInt16(array, 0);
                 }
-                result = BitConverter.ToUInt32(array, 0);
+                else
+                {
+                    result = BitConverter.ToUInt16(data, offset);
+                }
             }
             return result;
-        }
-
-        public static byte[] ToBytes(uint num)
-        {
-            byte[] bytes = BitConverter.GetBytes(num);
-            if (BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(bytes);
-            }
-            return bytes;
         }
 
         public static int ToInt32(byte[] data)
@@ -158,23 +143,13 @@ namespace LywGames.Common
             return result;
         }
 
-        public static byte[] ToBytes(int num)
+        public static uint ToUInt32(byte[] data)
         {
-            byte[] bytes = BitConverter.GetBytes(num);
-            if (BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(bytes);
-            }
-            return bytes;
-        }
-
-        public static ushort ToUInt16(byte[] data)
-        {
-            byte[] array = ToFixedLength(data, 2);
-            ushort result;
+            byte[] array = ToFixedLength(data, 4);
+            uint result;
             if (array == null)
             {
-                result = 0;
+                result = 0u;
             }
             else
             {
@@ -182,39 +157,57 @@ namespace LywGames.Common
                 {
                     Array.Reverse(array);
                 }
-                result = BitConverter.ToUInt16(array, 0);
+                result = BitConverter.ToUInt32(array, 0);
             }
             return result;
         }
 
-        public static ushort ToUInt16(byte[] data, int offset)
+        public static long ToInt64(byte[] data)
         {
-            ushort result;
-            if (data.Length <= offset)
+            byte[] array = ToFixedLength(data, 8);
+            long result;
+            if (array == null)
             {
-                result = 0;
+                result = 0L;
             }
             else
             {
                 if (BitConverter.IsLittleEndian)
                 {
-                    byte[] array = new byte[]
-                    {
-                        0,
-                        data[offset]
-                    };
-                    if (data.Length - offset > 1)
-                    {
-                        array[0] = data[offset + 1];
-                    }
-                    result = BitConverter.ToUInt16(array, 0);
+                    Array.Reverse(array);
                 }
-                else
-                {
-                    result = BitConverter.ToUInt16(data, offset);
-                }
+                result = BitConverter.ToInt64(array, 0);
             }
             return result;
+        }
+
+        public static ulong ToUInt64(byte[] data)
+        {
+            byte[] array = ToFixedLength(data, 8);
+            ulong result;
+            if (array == null)
+            {
+                result = 0uL;
+            }
+            else
+            {
+                if (BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(array);
+                }
+                result = BitConverter.ToUInt64(array, 0);
+            }
+            return result;
+        }
+
+        public static byte[] ToBytes(short num)
+        {
+            byte[] bytes = BitConverter.GetBytes(num);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
         }
 
         public static byte[] ToBytes(ushort num)
@@ -227,26 +220,37 @@ namespace LywGames.Common
             return bytes;
         }
 
-        public static short ToInt16(byte[] data)
+        public static byte[] ToBytes(int num)
         {
-            byte[] array = ToFixedLength(data, 2);
-            short result;
-            if (array == null)
+            byte[] bytes = BitConverter.GetBytes(num);
+            if (BitConverter.IsLittleEndian)
             {
-                result = 0;
+                Array.Reverse(bytes);
             }
-            else
-            {
-                if (BitConverter.IsLittleEndian)
-                {
-                    Array.Reverse(array);
-                }
-                result = BitConverter.ToInt16(array, 0);
-            }
-            return result;
+            return bytes;
         }
 
-        public static byte[] ToBytes(short num)
+        public static byte[] ToBytes(uint num)
+        {
+            byte[] bytes = BitConverter.GetBytes(num);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
+        }
+
+        public static byte[] ToBytes(long num)
+        {
+            byte[] bytes = BitConverter.GetBytes(num);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(bytes);
+            }
+            return bytes;
+        }
+
+        public static byte[] ToBytes(ulong num)
         {
             byte[] bytes = BitConverter.GetBytes(num);
             if (BitConverter.IsLittleEndian)
